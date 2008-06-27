bc0f358e15b03cafe6c8a6521b84a5f088f4d2f0
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2008-06-27 04:52:03 +0000

    added simple test for oaipmhharvester

---
+++ solr/code/arrow-portal/harvest-moon/src/test/java/au/edu/usq/solr/harvest/impl/OaiPmhHarvesterTest.java
@@ -0,0 +1,23 @@
+package au.edu.usq.solr.harvest.impl;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import au.edu.usq.solr.harvest.Item;
+
+public class OaiPmhHarvesterTest {
+
+    @Test
+    public void getItems() throws Exception {
+        OaiPmhHarvester h = new OaiPmhHarvester(
+            "http://rubric-vitalnew.usq.edu.au:8080/fedora/oai");
+        List<Item> items = h.getItems(null);
+        Assert.assertEquals(100, items.size());
+        Item item1 = items.get(0);
+        Assert.assertEquals("oai:rubric-vitalnew.usq.edu.au:uon:1",
+            item1.getId());
+    }
+
+}
