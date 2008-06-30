b7b1dc5b5f755b93389e9f5e0a42f0e488cd4e60
mccallum <mccallum@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2008-06-30 06:55:50 +0000

    Fetched the ResMapUrls and returned a list, still investigating different ways to access and manipulate ResMaps refs #68

--- solr/code/arrow-portal/harvest-moon/src/test/java/au/edu/usq/solr/harvest/impl/OaiOreHarvesterTest.java
+++ solr/code/arrow-portal/harvest-moon/src/test/java/au/edu/usq/solr/harvest/impl/OaiOreHarvesterTest.java
@@ -1,31 +1,30 @@
 package au.edu.usq.solr.harvest.impl;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import au.edu.usq.solr.harvest.Item;
-
 public class OaiOreHarvesterTest {
 
     @Test
-    public void getResMapUrls() throws Exception {
-        OaiOreHarvester harvester = new OaiOreHarvester(
-            "http://rspilot.usq.edu.au/cgi/search/simple/export_rspilot_ResMapUrls.xml?exp=0|1|-date/creators_name/title|archive|-|q:_fulltext_/abstract/creators_name/date/title:ALL:IN:the|-|eprint_status:eprint_status:ALL:EQ:archive|metadata_visibility:metadata_visibility:ALL:EX:show&output=ResMapUrls&_action_export=1&screen=Public::EPrintSearch&cache=47485")
-        List<ResMapUrl> resMapUrls = harvester.fetchResMapUrls()
-        Assert.assertEquals(14, resMapUrls.size())
-        ResMapUrl url1 = resMapUrls(0)
-        Assert.assertEquals(
-            "http://rspilot.usq.edu.au/cgi/export/11/ResMap/rspilot-eprint-11.xml", url1.)
+    public void fetchResMapUrls() throws Exception {
+        String hUrl = "http://rspilot.usq.edu.au/cgi/search/simple/export_rspilot_ResMapUrls.xml?exp=0|1|-date/creators_name/title|archive|-|q:_fulltext_/abstract/creators_name/date/title:ALL:IN:the|-|eprint_status:eprint_status:ALL:EQ:archive|metadata_visibility:metadata_visibility:ALL:EX:show&output=ResMapUrls&_action_export=1&screen=Public::EPrintSearch&cache=47485";
+        OaiOreHarvester harvester = new OaiOreHarvester(hUrl);
+        ArrayList<String> testArrayList = harvester.fetchResMapUrls();
+        Assert.assertEquals(14, testArrayList.size());
+
     }
 
     @Test
-    public void getOreItems() throws Exception {
-        OaiOreHarvester harvester = new OaiOreHarvester(
-            "http://rspilot.usq.edu.au/cgi/search/simple/export_rspilot_ResMapUrls.xml?exp=0|1|-date/creators_name/title|archive|-|q:_fulltext_/abstract/creators_name/date/title:ALL:IN:the|-|eprint_status:eprint_status:ALL:EQ:archive|metadata_visibility:metadata_visibility:ALL:EX:show&output=ResMapUrls&_action_export=1&screen=Public::EPrintSearch&cache=47485");
-        List<Item> items = harvester.getItems();
-        Assert.assertEquals(10, items.size());
+    public void serializeResMap() throws Exception {
+        String url = "http://rspilot.usq.edu.au/cgi/export/11/ResMap/rspilot-eprint-11.xml";
+        // ResourceMapFactory rmf = new ResourceMapFactory.class;
+        // ResourceMap rm = ResourceMapFa
+
+        // Integer ints = rm.getAggregation().numberOfResources();
+        // Assert.assertEquals("rta", "10", rm.getId());
+        // Assert.assertEquals("10", ints);
 
     }
 
