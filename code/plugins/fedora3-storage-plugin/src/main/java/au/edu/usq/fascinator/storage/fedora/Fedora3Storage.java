1920fc6ae803849975c24af88fa2182d99db43b0
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-06-17 04:02:57 +0000

    refs #216 addObject now returns an identifier. added copyright.

--- code/plugins/fedora3-storage-plugin/src/main/java/au/edu/usq/fascinator/storage/fedora/Fedora3Storage.java
+++ code/plugins/fedora3-storage-plugin/src/main/java/au/edu/usq/fascinator/storage/fedora/Fedora3Storage.java
@@ -1,3 +1,21 @@
+/* 
+ * The Fascinator - Fedora Commons 3.x storage plugin
+ * Copyright (C) 2009 University of Southern Queensland
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package au.edu.usq.fascinator.storage.fedora;
 
 import java.io.File;
@@ -74,28 +92,32 @@ public class Fedora3Storage implements Storage {
         // Don't need to do anything
     }
 
-    public void addObject(DigitalObject object) throws StorageException {
+    public String addObject(DigitalObject object) throws StorageException {
+        String fedoraId = null;
+        String oid = object.getId();
         try {
-            String fedoraId = getFedoraId(object.getId());
+            fedoraId = getFedoraId(oid);
             if (fedoraId == null) {
                 log.debug("Creating object {}", fedoraId);
-                client.createObject(object.getId(), DEFAULT_NAMESPACE);
+                fedoraId = client.createObject("", DEFAULT_NAMESPACE);
+                log.debug("Client returned Fedora PID: {}", fedoraId);
             } else {
                 log.debug("Updating object {}", fedoraId);
             }
             for (Payload payload : object.getPayloadList()) {
-                addPayload(object.getId(), payload);
+                addPayload(oid, payload);
             }
         } catch (Exception e) {
             throw new StorageException("Failed to add object", e);
         }
+        return fedoraId;
     }
 
     public void removeObject(String oid) {
         try {
             client.purgeObject(getFedoraId(oid));
         } catch (IOException ioe) {
-            ioe.printStackTrace();
+            log.error("Failed to remove object {}", ioe);
         }
     }
 
@@ -133,6 +155,10 @@ public class Fedora3Storage implements Storage {
     }
 
     public Payload getPayload(String oid, String pid) {
+        DigitalObject object = getObject(oid);
+        if (object != null) {
+            return object.getPayload(pid);
+        }
         return null;
     }
 
