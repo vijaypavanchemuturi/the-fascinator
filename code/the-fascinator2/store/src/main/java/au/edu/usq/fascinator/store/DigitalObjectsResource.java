c7f22e1444eac67830530e09a5c626c5a11b8d7f
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-06-18 02:02:19 +0000

    refs #219 added DELETE handler

--- code/the-fascinator2/store/src/main/java/au/edu/usq/fascinator/store/DigitalObjectsResource.java
+++ code/the-fascinator2/store/src/main/java/au/edu/usq/fascinator/store/DigitalObjectsResource.java
@@ -27,6 +27,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.zip.ZipFile;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -52,23 +53,9 @@ import au.edu.usq.fascinator.api.Storage;
 import au.edu.usq.fascinator.api.StorageException;
 import au.edu.usq.fascinator.common.ConfigUtils;
 
-/* 
- * The Fascinator - Fedora Commons 3.x storage plugin
- * Copyright (C) 2009 University of Southern Queensland
+/**
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * @author Oliver Lucido
  */
 @Path("/objects")
 public class DigitalObjectsResource {
@@ -158,6 +145,13 @@ public class DigitalObjectsResource {
             urise.printStackTrace();
         }
         return null;// Response.created(location).build();
+    }
+
+    @DELETE
+    @Path("{objectId}")
+    public Response handleDelete(@PathParam("objectId") String objectId) {
+        storage.removeObject(objectId);
+        return Response.ok().build();
     }
 
 }
