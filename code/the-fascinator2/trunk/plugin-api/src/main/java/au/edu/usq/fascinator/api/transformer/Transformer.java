8ca618b543ad773b554d41747aeb45cb6f95fd73
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-07-09 06:35:08 +0000

    updated transformer api for files

--- code/the-fascinator2/trunk/plugin-api/src/main/java/au/edu/usq/fascinator/api/transformer/Transformer.java
+++ code/the-fascinator2/trunk/plugin-api/src/main/java/au/edu/usq/fascinator/api/transformer/Transformer.java
@@ -1,13 +1,11 @@
 package au.edu.usq.fascinator.api.transformer;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.File;
 
 import au.edu.usq.fascinator.api.Plugin;
 
 public interface Transformer extends Plugin {
 
-    public void transform(InputStream in, OutputStream out)
-            throws TransformerException;
+    public File transform(File in) throws TransformerException;
 
 }
