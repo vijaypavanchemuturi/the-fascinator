fb7a17a8e27de9c2ed2895d3944f5848e11c0bde
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-07-15 06:59:49 +0000

    refs #258 rdf rules

--- code/the-fascinator2/trunk/core/src/test/resources/local-files.py
+++ code/the-fascinator2/trunk/core/src/test/resources/local-files.py
@@ -1,5 +1,6 @@
 import time
 from au.edu.usq.fascinator.indexer.rules import AddField, New
+from org.dom4j.io import SAXReader
 
 #
 # Available objects:
@@ -9,6 +10,11 @@ from au.edu.usq.fascinator.indexer.rules import AddField, New
 #    payloadId : Payload identifier
 #    storageId : Storage layer identifier
 #
+
+def indexNode(doc, name, xPath):
+    node = doc.selectSingleNode(xPath)
+    if node is not None:
+        rules.add(AddField(name, node.getText()))
 
 rules.add(New())
 
@@ -29,3 +35,45 @@ rules.add(AddField("item_class", "document"))
 
 rules.add(AddField("repository_name", params["repository.name"]))
 rules.add(AddField("repository_type", params["repository.type"]))
+
+rdfString = object.getPayload("rdf").getInputStream()  #in future use object.getMetadata("rdf")
+#rdfString = object.getMetadata().getInputStream()
+if rdfString is not None:
+    saxReader = SAXReader()
+    rdfXml = saxReader.read(rdfString)
+    
+    rootNode = rdfXml.getRootElement()
+    
+    ns = rootNode.getNamespace().getURI()
+    
+    #j.2:pageCount for pdf and no dc
+    
+    if rootNode.getNamespaceForPrefix("dc"):
+        indexNode(rootNode, "dc.title", "./rdf:Description/dc:title")
+        indexNode(rootNode, "dc.subject", "./rdf:Description/dc:title")
+        indexNode(rootNode, "dc.date", "./rdf:Description/dc:date")
+        indexNode(rootNode, "dc.author", "./rdf:Description/dc:creator")
+    
+    if rootNode.getNamespaceForPrefix("j.1"):
+        indexNode(rootNode, "dc.creator", "./rdf:Description/j.1:initial-creator")
+        indexNode(rootNode, "dc.creator", "./rdf:Description/j.1:fullname")
+        
+    if rootNode.getNamespaceForPrefix("j.0"):
+        indexNode(rootNode, "dc.title", "./rdf:Description/j.0:title")
+        indexNode(rootNode, "full_text", "./rdf:Description/j.0:plainTextContent")
+        indexNode(rootNode, "dc.format", "./rdf:Description/j.0:mimeType")
+        indexNode(rootNode, "dc.creationdate", "./rdf:Description/j.1:creation-date")
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
