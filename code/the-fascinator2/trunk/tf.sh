24521e2492cec40e481b9a189d5b1f9ca794491e
dickinso <dickinso@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-08-17 05:52:25 +0000

    Updated script, added harvest

--- code/the-fascinator2/trunk/tf.sh
+++ code/the-fascinator2/trunk/tf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-./tf_env.sh
+. tf_env.sh
 
-if [ "$1" == "check" ]; then
+if [ "$1" == "" ]; then
+	echo "Usage: ./tf.sh start|stop|restart|check"
+elif [ "$1" == "check" ]; then
     echo "Are these the droids you're looking for?"
 	pgrep -l -f jetty
 	pgrep -l -f "java -jar start.jar"
-fi
-
-if [ "$1" == "stop" ]; then
+elif [ "$1" == "stop" ]; then
 	echo "Stopping..."
 	pkill -f "java -jar start.jar"
 	pkill -f jetty
@@ -16,12 +16,12 @@ fi
 
 if [ "$1" == "start" -o "$1" == "restart" ]; then
 	echo "Starting SOLR"
-	cd /opt/the-fascinator2/solr
+	cd $FASCINATOR_HOME/solr
 	java -jar start.jar &>$OLDPWD/solr.out &
 	cd $OLDPWD
 	
 	echo "Starting Portal"
-	cd /opt/the-fascinator2/code/portal2
+	cd $FASCINATOR_HOME/code/portal2
 	mvn -Dhttp.nonProxyHosts=localhost -P test -Djetty.port=9997 jetty:run &>$OLDPWD/portal.out &
 	cd $OLDPWD
 fi
