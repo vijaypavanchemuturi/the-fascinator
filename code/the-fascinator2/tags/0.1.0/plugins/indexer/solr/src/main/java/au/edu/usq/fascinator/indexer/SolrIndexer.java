a892f347f05d634680853597f87d94103e9ef709
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-08-24 06:34:21 +0000

    [maven-release-plugin]  copy for tag 0.1.0

---
+++ code/the-fascinator2/tags/0.1.0/plugins/indexer/solr/src/main/java/au/edu/usq/fascinator/indexer/SolrIndexer.java
@@ -0,0 +1,398 @@
+/* 
+ * The Fascinator - Indexer
+ * Copyright (C) 2009 University of Southern Queensland
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package au.edu.usq.fascinator.indexer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.io.IOUtils;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.request.DirectXmlRequest;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentFactory;
+import org.dom4j.io.SAXReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.edu.usq.fascinator.api.PluginException;
+import au.edu.usq.fascinator.api.PluginManager;
+import au.edu.usq.fascinator.api.indexer.Indexer;
+import au.edu.usq.fascinator.api.indexer.IndexerException;
+import au.edu.usq.fascinator.api.indexer.SearchRequest;
+import au.edu.usq.fascinator.api.indexer.rule.RuleException;
+import au.edu.usq.fascinator.api.storage.DigitalObject;
+import au.edu.usq.fascinator.api.storage.Payload;
+import au.edu.usq.fascinator.api.storage.Storage;
+import au.edu.usq.fascinator.api.storage.StorageException;
+import au.edu.usq.fascinator.common.JsonConfig;
+
+public class SolrIndexer implements Indexer {
+
+    private Logger log = LoggerFactory.getLogger(SolrIndexer.class);
+
+    private JsonConfig config;
+
+    private Storage storage;
+
+    private CommonsHttpSolrServer solr;
+
+    private boolean autoCommit;
+
+    private String propertiesId;
+
+    private List<File> tempFiles;
+
+    private SAXReader saxReader;
+
+    private Map<String, String> namespaces;
+
+    private String username;
+    private String password;
+
+    public String getId() {
+        return "solr";
+    }
+
+    public String getName() {
+        return "Apache Solr Indexer";
+    }
+
+    public void init(File jsonFile) throws IndexerException {
+        try {
+            config = new JsonConfig(jsonFile);
+        } catch (IOException ioe) {
+            throw new IndexerException(ioe);
+        }
+
+        String storageType = config.get("storage/type");
+        try {
+            storage = PluginManager.getStorage(storageType);
+            storage.init(jsonFile);
+        } catch (PluginException pe) {
+            log.error("Failed to load storage plugin: {}", storageType);
+        }
+
+        try {
+            URI solrUri = new URI(config.get("indexer/solr/uri"));
+            solr = new CommonsHttpSolrServer(solrUri.toURL());
+            username = config.get("indexer/solr/username");
+            password = config.get("indexer/solr/password");
+            if (username != null && password != null) {
+                UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(
+                        username, password);
+                HttpClient hc = solr.getHttpClient();
+                hc.getParams().setAuthenticationPreemptive(true);
+                hc.getState().setCredentials(AuthScope.ANY, credentials);
+            }
+        } catch (MalformedURLException mue) {
+            log.error("Malformed URL", mue);
+        } catch (URISyntaxException urise) {
+            log.error("Invalid URI", urise);
+        }
+
+        autoCommit = Boolean.parseBoolean(config.get("indexer/solr/autocommit",
+                "true"));
+        propertiesId = config.get("indexer/propertiesId", "SOF-META");
+
+        namespaces = new HashMap<String, String>();
+        DocumentFactory.getInstance().setXPathNamespaceURIs(namespaces);
+        saxReader = new SAXReader();
+    }
+
+    @Override
+    public void shutdown() throws PluginException {
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+
+    @Override
+    public void search(SearchRequest request, OutputStream response)
+            throws IndexerException {
+        SolrSearcher searcher = new SolrSearcher(solr.getBaseURL());
+        if (username != null && password != null) {
+            searcher.authenticate(username, password);
+        }
+        InputStream result;
+        try {
+            StringBuilder extras = new StringBuilder();
+            for (String name : request.getParamsMap().keySet()) {
+                for (String param : request.getParams(name)) {
+                    extras.append(name);
+                    extras.append("=");
+                    extras.append(URLEncoder.encode(param, "UTF-8"));
+                    extras.append("&");
+                }
+            }
+            extras.append("wt=json");
+            result = searcher.get(request.getQuery(), extras.toString(), false);
+            IOUtils.copy(result, response);
+            result.close();
+        } catch (IOException ioe) {
+            throw new IndexerException(ioe);
+        }
+    }
+
+    @Override
+    public void remove(String oid) throws IndexerException {
+        log.debug("Deleting " + oid + " from index");
+        try {
+            solr.deleteByQuery("oid:\"" + oid + "\"");
+            solr.commit();
+        } catch (SolrServerException sse) {
+            throw new IndexerException(sse);
+        } catch (IOException ioe) {
+            throw new IndexerException(ioe);
+        }
+    }
+
+    @Override
+    public void remove(String oid, String pid) throws IndexerException {
+        log.debug("Deleting {}::{} from index", oid, pid);
+        try {
+            solr.deleteByQuery("oid:\"" + oid + "\" AND pid:\"" + pid + "\"");
+            solr.commit();
+        } catch (SolrServerException sse) {
+            throw new IndexerException(sse);
+        } catch (IOException ioe) {
+            throw new IndexerException(ioe);
+        }
+    }
+
+    @Override
+    public void index(String oid) throws IndexerException {
+        List<Payload> payloadList = storage.getObject(oid).getPayloadList();
+        for (Payload payload : payloadList) {
+            index(oid, payload.getId());
+        }
+    }
+
+    @Override
+    public void index(String oid, String pid) throws IndexerException {
+        if (propertiesId.equals(pid)) {
+            return;
+        }
+        log.info("Indexing " + oid + "/" + pid);
+
+        // get the indexer properties or we can't index
+        Properties props = getIndexerProperties(oid);
+        if (props == null) {
+            log.warn("Indexer properties not found, object not indexed");
+            throw new IndexerException("Indexer properties not found");
+        }
+
+        try {
+            // create the item for indexing
+            String objectId = props.getProperty("oid", oid);
+            DigitalObject object = storage.getObject(objectId);
+
+            // get the indexer rules
+            String rulesPid = props.getProperty("rulesOid");
+            File rules = createTempFile("rules", ".script");
+            FileOutputStream rulesOut = new FileOutputStream(rules);
+            Payload rulesScript = storage.getPayload(rulesPid, props
+                    .getProperty("rulesPid"));
+            IOUtils.copy(rulesScript.getInputStream(), rulesOut);
+            rulesOut.close();
+
+            // primary metadata datastream
+            String metaPid = props.getProperty("metaPid", "DC");
+            object = new IndexerDigitalObject(object, metaPid);
+
+            // index the object
+            String set = null; // TODO
+            File solrFile = null;
+            if (metaPid.equals(pid)) {
+                solrFile = indexMetadata(object, oid, set, rules, props);
+            } else {
+                solrFile = indexDatastream(object, oid, pid, rules, props);
+            }
+            if (solrFile != null) {
+                InputStream inputDoc = new FileInputStream(solrFile);
+                String xml = IOUtils.toString(inputDoc);
+                inputDoc.close();
+                SolrRequest update = new DirectXmlRequest("/update", xml);
+                solr.request(update);
+                if (autoCommit) {
+                    solr.commit();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Indexing failed!\n-----\n{}\n-----\n", e.getMessage());
+        } finally {
+            cleanupTempFiles();
+        }
+    }
+
+    private File indexMetadata(DigitalObject item, String pid, String set,
+            File rulesFile, Properties props) throws IOException,
+            StorageException, RuleException {
+        log.info("Indexing metadata...");
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Payload ds = item.getPayload(props.getProperty("metaPid"));
+        IOUtils.copy(ds.getInputStream(), out);
+        InputStreamReader in = new InputStreamReader(new ByteArrayInputStream(
+                out.toByteArray()), "UTF-8");
+        return index(item, pid, null, set, in, rulesFile, props);
+    }
+
+    private File indexDatastream(DigitalObject item, String pid, String dsId,
+            File rulesFile, Properties props) throws IOException, RuleException {
+        log.info("Indexing datastream...");
+        Reader in = new StringReader("<add><doc/></add>");
+        return index(item, pid, dsId, null, in, rulesFile, props);
+    }
+
+    private File index(DigitalObject object, String sid, String pid,
+            String set, Reader in, File ruleScript, Properties props)
+            throws IOException, RuleException {
+        File solrFile = createTempFile("solr", ".xml");
+        Writer out = new OutputStreamWriter(new FileOutputStream(solrFile),
+                "UTF-8");
+        try {
+            String engineName = props.getProperty("scriptType", "python");
+            ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+            ScriptEngine scriptEngine = scriptEngineManager
+                    .getEngineByName(engineName);
+            if (scriptEngine == null) {
+                throw new RuleException("No script engine found for '"
+                        + engineName + "'");
+            }
+            RuleManager rules = new RuleManager();
+            scriptEngine.put("indexer", this);
+            scriptEngine.put("rules", rules);
+            scriptEngine.put("object", object);
+            scriptEngine.put("payloadId", pid);
+            scriptEngine.put("storageId", sid);
+            scriptEngine.put("params", props);
+            scriptEngine.put("isMetadata", pid == null);
+            // TODO add required solr fields?
+            scriptEngine.eval(new FileReader(ruleScript));
+            rules.run(in, out);
+            if (rules.cancelled()) {
+                log.info("Indexing rules were cancelled");
+                return null;
+            }
+        } catch (Exception e) {
+            throw new RuleException(e);
+        } finally {
+            in.close();
+            out.close();
+        }
+        return solrFile;
+    }
+
+    private Properties getIndexerProperties(String oid) {
+        try {
+            Payload sofMeta = storage.getPayload(oid, propertiesId);
+            Properties props = new Properties();
+            props.load(sofMeta.getInputStream());
+            return props;
+        } catch (Exception ioe) {
+            log.warn("Failed to load properties", ioe);
+        }
+        return null;
+    }
+
+    private File createTempFile(String prefix, String postfix)
+            throws IOException {
+        File tempFile = File.createTempFile(prefix, postfix);
+        if (tempFiles == null) {
+            tempFiles = new ArrayList<File>();
+        }
+        tempFiles.add(tempFile);
+        return tempFile;
+    }
+
+    private void cleanupTempFiles() {
+        for (File tempFile : tempFiles) {
+            if (tempFile.exists()) {
+                tempFile.delete();
+            }
+        }
+        tempFiles = new ArrayList<File>();
+    }
+
+    // Helper methods
+
+    public InputStream getResource(String path) {
+        return getClass().getResourceAsStream(path);
+    }
+
+    public Document getXmlDocument(Payload payload) {
+        try {
+            return getXmlDocument(payload.getInputStream());
+        } catch (IOException ioe) {
+            log.error("Failed to parse XML", ioe);
+        }
+        return null;
+    }
+
+    public Document getXmlDocument(InputStream xmlIn) {
+        try {
+            return saxReader.read(new InputStreamReader(xmlIn, "UTF-8"));
+        } catch (UnsupportedEncodingException uee) {
+        } catch (DocumentException de) {
+            log.error("Failed to parse XML", de);
+        }
+        return null;
+    }
+
+    public void registerNamespace(String prefix, String uri) {
+        namespaces.put(prefix, uri);
+    }
+
+    public void unregisterNamespace(String prefix) {
+        namespaces.remove(prefix);
+    }
+
+}
---
+++ code/the-fascinator2/tags/0.1.0/portal/src/main/config/portal/default/scripts/detail.py
@@ -0,0 +1,112 @@
+from au.edu.usq.fascinator.api.indexer import SearchRequest
+from au.edu.usq.fascinator.api.storage import PayloadType
+from au.edu.usq.fascinator.common import JsonConfigHelper
+from java.net import URLDecoder
+from java.io import ByteArrayInputStream, ByteArrayOutputStream, StringWriter
+from org.dom4j.io import OutputFormat, XMLWriter, SAXReader
+
+from org.apache.commons.io import IOUtils;
+from au.edu.usq.fascinator.model import DCRdf 
+
+class SolrDoc:
+    def __init__(self, json):
+        self.json = json
+    
+    def getField(self, name):
+        return self.json.getList("response/docs/%s" % name).get(0)
+    
+    def getFieldText(self, name):
+        return self.json.get("response/docs/%s" % name)
+    
+    def getFieldList(self, name):
+        return self.json.getList("response/docs/%s" % name)
+    
+    def getDublinCore(self):
+        dc = self.json.getList("response/docs").get(0)
+        remove = ["dc_title", "dc_description"]
+        for entry in dc:
+            if not entry.startswith("dc_"):
+                remove.append(entry)
+        for key in remove:
+            dc.remove(key)
+        return JsonConfigHelper(dc).getMap("/")
+    
+    def toString(self):
+        return self.json.toString()
+
+
+class DetailData:
+    def __init__(self):
+        self.__storage = Services.storage
+        uri = request.getAttribute("RequestURI")
+        basePath = portalId + "/" + pageName
+        self.__oid = URLDecoder.decode(uri[len(basePath)+1:])
+        self.__dcRdf = None
+        self.__metadata = JsonConfigHelper()
+        self.__search()
+    
+    def __search(self):
+        req = SearchRequest('id:"%s"' % self.__oid)
+        out = ByteArrayOutputStream()
+        Services.indexer.search(req, out)
+        json = JsonConfigHelper(ByteArrayInputStream(out.toByteArray()))
+        self.__metadata = SolrDoc(json)
+    
+    def formatName(self, name):
+        return name[3:4].upper() + name[4:]
+    
+    def formatValue(self, value):
+        return value[1:-1]
+    
+    def isHidden(self, pid):
+        if pid.find("_files%2F")>-1:
+            return True
+        return False
+    
+    def getMetadata(self):
+        return self.__metadata
+    
+    def getObject(self):
+        print " *** getting %s" % self.__oid 
+        return self.__storage.getObject(self.__oid)
+    
+    def getDcRdf(self):
+        print " *** payload list: %s *** " % self.__storage.getObject(self.__oid).payloadList
+        dcrdfPayload = self.__storage.getPayload(self.__oid, "dc-rdf")
+        if dcrdfPayload is not None:
+            self.__dcRdf = DCRdf(dcrdfPayload.getInputStream())
+        return self.__dcRdf
+    
+    def getPayloadContent(self):
+        format = self.__metadata.getField("dc_format")
+        slash = self.__oid.rfind("/")
+        pid = self.__oid[slash+1:]
+        print " *** payload content, format: %s, pid: %s *** " % (format, pid)
+        contentStr = ""
+        if format.startswith("text"):
+            contentStr = "<pre>"
+            payload = self.__storage.getPayload(self.__oid, pid)
+            str = StringWriter() 
+            IOUtils.copy(payload.getInputStream(), str)
+            contentStr += str.toString()
+            contentStr += "</pre>"
+        elif format.find("vnd.ms-")>-1 or format.find("vnd.oasis.opendocument.")>-1:
+            #get the html version if exist....
+            pid = pid[:pid.find(".")] + ".htm"
+            payload = self.__storage.getPayload(self.__oid, pid)
+            saxReader = SAXReader()
+            document = saxReader.read(payload.getInputStream())
+            slideNode = document.selectSingleNode("//div[@class='body']")
+            #linkNodes = slideNode.selectNodes("//img")
+            #contentStr = slideNode.asXML();
+            # encode character entities correctly
+            out = ByteArrayOutputStream()
+            format = OutputFormat.createPrettyPrint()
+            format.setSuppressDeclaration(True)
+            writer = XMLWriter(out, format)
+            writer.write(slideNode)
+            writer.close()
+            contentStr = out.toString("UTF-8")
+        return contentStr
+
+scriptObject = DetailData()
