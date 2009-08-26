9a56b2a670fc89c0d88b69e2be9a830d1d478a03
dickinso <dickinso@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-08-26 09:18:54 +0000

    Tidied up general construct - external string file now works

--- code/the-fascinator2/contrib/archetypes/archetype-plugin-harvester/src/main/resources/archetype-resources/src/main/java/Messages.java
+++ code/the-fascinator2/contrib/archetypes/archetype-plugin-harvester/src/main/resources/archetype-resources/src/main/java/Messages.java
@@ -21,7 +21,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "${package}.messages.properties";
+	private static final String BUNDLE_NAME = "${package}.messages";
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
