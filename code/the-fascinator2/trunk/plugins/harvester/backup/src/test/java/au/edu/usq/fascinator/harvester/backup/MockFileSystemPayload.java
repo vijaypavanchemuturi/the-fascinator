3f6159a97b02c9029b48a0e820a31dfd812f3ce5
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-09-23 06:40:03 +0000

    refs #389 backup functionality merge to trunk

---
+++ code/the-fascinator2/trunk/plugins/harvester/backup/src/test/java/au/edu/usq/fascinator/harvester/backup/MockFileSystemPayload.java
@@ -0,0 +1,130 @@
+/* 
+ * The Fascinator - File System storage plugin
+ * Copyright (C) 2009 University of Southern Queensland
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package au.edu.usq.fascinator.harvester.backup;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.edu.usq.fascinator.api.storage.Payload;
+import au.edu.usq.fascinator.api.storage.PayloadType;
+import au.edu.usq.fascinator.common.MimeTypeUtil;
+import au.edu.usq.fascinator.common.storage.impl.GenericPayload;
+
+public class MockFileSystemPayload extends GenericPayload {
+
+    private Logger log = LoggerFactory.getLogger(MockFileSystemPayload.class);
+
+    private File file;
+
+    private File homeDir;
+
+    public MockFileSystemPayload(File homeDir, Payload payload) {
+        super(payload);
+        this.homeDir = homeDir;
+        updateMeta();
+    }
+
+    public MockFileSystemPayload(File homeDir, File payloadFile) {
+        this.homeDir = homeDir;
+        file = payloadFile;
+        if (file.isAbsolute()) {
+            setId(payloadFile.getName());
+        } else {
+            setId(payloadFile.getPath());
+        }
+        updateMeta();
+    }
+
+    private void updateMeta() {
+        // set default payload meta if not already set
+        if (getType() == null) {
+            setType(PayloadType.Data);
+        }
+        if (getLabel() == null) {
+            setLabel(getFile().getAbsolutePath());
+        }
+        if (getContentType() == null) {
+            setContentType(MimeTypeUtil.getMimeType(getFile()));
+        }
+        File metaFile = new File(getFile().getParentFile(), getFile().getName()
+                + ".meta");
+        try {
+            Properties props = new Properties();
+            if (metaFile.exists()) {
+                Reader metaReader = new FileReader(metaFile);
+                props.load(metaReader);
+                metaReader.close();
+                setId(props.getProperty("id", getId()));
+                setType(PayloadType.valueOf(props.getProperty("payloadType",
+                        getType().toString())));
+                setLabel(props.getProperty("label", getId()));
+                setContentType(props.getProperty("contentType",
+                        getContentType()));
+            } else {
+                metaFile.getParentFile().mkdirs();
+                OutputStream metaOut = new FileOutputStream(metaFile);
+                props.setProperty("id", getId());
+                props.setProperty("payloadType", getType().toString());
+                props.setProperty("label", getLabel());
+                props.setProperty("contentType", getContentType());
+                props.store(metaOut, "Payload metadata for "
+                        + getFile().getAbsolutePath());
+                metaOut.close();
+            }
+        } catch (IOException ioe) {
+            log.warn("Failed to read/write metaFile {}: {}", metaFile, ioe
+                    .getMessage());
+        }
+    }
+
+    public File getFile() {
+        if (file == null) {
+            return new File(homeDir, getId());
+        } else {
+            if (file.isAbsolute()) {
+                return file;
+            } else {
+                return new File(homeDir, file.getPath());
+            }
+        }
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        if (file == null) {
+            return super.getInputStream();
+        }
+        return new FileInputStream(getFile());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%s)", getLabel(), getId());
+    }
+}
