8fed64fd11dd8fe4888547537d505e952a2eca7a
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-10-20 04:48:17 +0000

    refs #406 added more facets, consistent left hand menu, stub help page

--- code/the-fascinator2/branches/policies2/portal/src/main/config/portal/default/scripts/home.py
+++ code/the-fascinator2/branches/policies2/portal/src/main/config/portal/default/scripts/home.py
@@ -1,3 +1,6 @@
+from au.edu.usq.fascinator.api.indexer import SearchRequest
+from au.edu.usq.fascinator.common import JsonConfigHelper
+from java.io import ByteArrayInputStream, ByteArrayOutputStream
 
 class HomeData:
     def __init__(self):
@@ -6,5 +9,20 @@ class HomeData:
             portalName = formData.get("value")
             print " * home.py: delete portal %s" % portalName
             Services.portalManager.remove(portalName)
+        self.__latest = JsonConfigHelper()
+        self.__search()
     
+    def __search(self):
+        req = SearchRequest("d_usq_document_effective_date:[NOW-1MONTH TO *]")
+        req.setParam("fq", 'item_type:"object"')
+        req.setParam("rows", "10")
+        
+        out = ByteArrayOutputStream()
+        indexer = Services.getIndexer()
+        indexer.search(req, out)
+        self.__latest = JsonConfigHelper(ByteArrayInputStream(out.toByteArray()))
+    
+    def getLatest(self):
+        return self.__latest.getList("response/docs")
+
 scriptObject = HomeData()
