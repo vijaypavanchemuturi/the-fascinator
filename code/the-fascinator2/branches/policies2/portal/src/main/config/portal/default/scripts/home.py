15ca9a619a035856bc662c82d2bf1643a4829057
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-10-26 07:17:34 +0000

    refs #427 updated as per feedback

--- code/the-fascinator2/branches/policies2/portal/src/main/config/portal/default/scripts/home.py
+++ code/the-fascinator2/branches/policies2/portal/src/main/config/portal/default/scripts/home.py
@@ -1,6 +1,7 @@
 from au.edu.usq.fascinator.api.indexer import SearchRequest
 from au.edu.usq.fascinator.common import JsonConfigHelper
 from java.io import ByteArrayInputStream, ByteArrayOutputStream
+from java.util import HashMap
 
 class HomeData:
     def __init__(self):
@@ -9,32 +10,48 @@ class HomeData:
             portalName = formData.get("value")
             print " * home.py: delete portal %s" % portalName
             Services.portalManager.remove(portalName)
-	self.__total = 0
+        self.__portal = Services.portalManager.get(portalId)
         self.__latest = JsonConfigHelper()
+        self.__result = JsonConfigHelper()
         self.__search()
     
     def __search(self):
         indexer = Services.getIndexer()
-
+        
         req = SearchRequest("d_usq_document_effective_date:[NOW-1MONTH TO *]")
         req.setParam("fq", 'item_type:"object"')
         req.setParam("rows", "10")
-	req.setParam("sort", "d_usq_document_effective_date asc, title_sort asc");
+        req.setParam("sort", "d_usq_document_effective_date asc, title_sort asc");
         out = ByteArrayOutputStream()
         indexer.search(req, out)
         self.__latest = JsonConfigHelper(ByteArrayInputStream(out.toByteArray()))
-
-	req = SearchRequest("*:*")
+        
+        req = SearchRequest("*:*")
         req.setParam("fq", 'item_type:"object"')
         req.setParam("rows", "0")
+        req.setParam("facet", "true")
+        req.setParam("facet.field", "f_usq_document_type")
         out = ByteArrayOutputStream()
         indexer.search(req, out)
-        self.__total = JsonConfigHelper(ByteArrayInputStream(out.toByteArray())).get("response/numFound")
+        self.__result = JsonConfigHelper(ByteArrayInputStream(out.toByteArray()))
     
     def getLatest(self):
         return self.__latest.getList("response/docs")
-
+    
     def getTotal(self):
-        return self.__total
+        return self.__result.get("response/numFound")
+    
+    def getFacetName(self, key):
+        return self.__portal.facetFields.get(key)
+    
+    def getFacetCounts(self, key):
+        values = HashMap()
+        valueList = self.__result.getList("facet_counts/facet_fields/%s" % key)
+        for i in range(0,len(valueList),2):
+            name = valueList[i]
+            count = valueList[i+1]
+            if count > 0:
+                values[name] = count
+        return values
 
 scriptObject = HomeData()
