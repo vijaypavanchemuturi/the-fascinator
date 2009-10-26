b7324fe6cafde6b9dd2a88659967bf0d3c1af3e9
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-10-26 01:34:58 +0000

    added total policy count on home page

--- code/the-fascinator2/branches/policies2/portal/src/main/config/portal/default/scripts/home.py
+++ code/the-fascinator2/branches/policies2/portal/src/main/config/portal/default/scripts/home.py
@@ -9,20 +9,32 @@ class HomeData:
             portalName = formData.get("value")
             print " * home.py: delete portal %s" % portalName
             Services.portalManager.remove(portalName)
+	self.__total = 0
         self.__latest = JsonConfigHelper()
         self.__search()
     
     def __search(self):
+        indexer = Services.getIndexer()
+
         req = SearchRequest("d_usq_document_effective_date:[NOW-1MONTH TO *]")
         req.setParam("fq", 'item_type:"object"')
         req.setParam("rows", "10")
-        
+	req.setParam("sort", "d_usq_document_effective_date asc, title_sort asc");
         out = ByteArrayOutputStream()
-        indexer = Services.getIndexer()
         indexer.search(req, out)
         self.__latest = JsonConfigHelper(ByteArrayInputStream(out.toByteArray()))
+
+	req = SearchRequest("*:*")
+        req.setParam("fq", 'item_type:"object"')
+        req.setParam("rows", "0")
+        out = ByteArrayOutputStream()
+        indexer.search(req, out)
+        self.__total = JsonConfigHelper(ByteArrayInputStream(out.toByteArray())).get("response/numFound")
     
     def getLatest(self):
         return self.__latest.getList("response/docs")
+
+    def getTotal(self):
+        return self.__total
 
 scriptObject = HomeData()
