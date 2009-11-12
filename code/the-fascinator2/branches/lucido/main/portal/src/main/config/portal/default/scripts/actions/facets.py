d36a4cceb7f0018935dd9e6acca1215b070bbdcd
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-11-12 01:24:33 +0000

    refs #423 finishing off portal/facet settings

--- code/the-fascinator2/branches/lucido/main/portal/src/main/config/portal/default/scripts/actions/facets.py
+++ code/the-fascinator2/branches/lucido/main/portal/src/main/config/portal/default/scripts/actions/facets.py
@@ -7,16 +7,24 @@ class FacetActions:
         portalManager = Services.getPortalManager()
         portal = portalManager.get(portalId)
         func = formData.get("func")
-        if func == "facets-update":
+        if func == "view-update":
+            portal.setDescription(formData.get("view-description"))
+            portal.setQuery(formData.get("view-query"))
+            portal.setRecordsPerPage(int(formData.get("view-records-per-page")))
+            portal.setFacetCount(int(formData.get("view-facet-count")))
+            portal.setFacetSort(formData.get("view-facet-sort") is not None)
+            portalManager.save(portal)
+        elif func == "facets-update":
             portal.removePath("portal/facet-fields")
             fields = formData.getValues("field")
             labels = formData.getValues("label")
             displays = formData.getValues("display")
             deletes = formData.getValues("delete")
             for i in range(0, len(fields)):
+                field = fields[i]
                 if deletes[i] == "false":
-                    portal.set("portal/facet-fields/%s/label" % fields[i], labels[i])
-                    portal.set("portal/facet-fields/%s/display" % fields[i], displays[i])
+                    portal.set("portal/facet-fields/%s/label" % field, labels[i])
+                    portal.set("portal/facet-fields/%s/display" % field, displays[i])
             portalManager.save(portal)
         writer = response.getPrintWriter("text/plain")
         writer.println(result)
