c52929f2698c920103a526d61d3557878b5b849a
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-11-12 04:39:02 +0000

    refs #436 presentations should now blog properly

--- code/the-fascinator2/branches/lucido/main/portal/src/main/config/portal/default/scripts/actions/blog.py
+++ code/the-fascinator2/branches/lucido/main/portal/src/main/config/portal/default/scripts/actions/blog.py
@@ -82,6 +82,7 @@ class AtomEntryPoster:
             doc = tidy.parseDOM(payload.getInputStream(), out)
             content = out.toString("UTF-8")
             content = self.__processMedia(oid, doc, content)
+            #print "[\n%s\n]" % content
         except Exception, e:
             print " * blog.py: Failed to get content: %s" % e.getMessage()
         return content
@@ -95,8 +96,12 @@ class AtomEntryPoster:
         links = doc.getElementsByTagName(elem)
         for i in range(0, links.getLength()):
             elem = links.item(i)
-            pid = elem.getAttribute(attr)
-            payload = Services.getStorage().getObject(oid).getPayload(pid)
+            attrValue = elem.getAttribute(attr)
+            pid = attrValue
+            payload = Services.getStorage().getPayload(oid, pid)
+            if payload is None:
+                pid = URLDecoder.decode(pid, "UTF-8")
+                payload = Services.getStorage().getPayload(oid, pid)
             if payload is not None:
                 #HACK to upload PDFs
                 contentType = payload.getContentType().replace("application/", "image/")
@@ -104,9 +109,11 @@ class AtomEntryPoster:
                                          payload.getInputStream())
                 if entry is not None:
                     id = entry.getId()
-                    print " * blog.py: replacing %s with %s" % (pid, id)
-                    content = content.replace('%s="%s"' % (attr, pid),
+                    print " * blog.py: replacing %s with %s" % (attrValue, id)
+                    content = content.replace('%s="%s"' % (attr, attrValue),
                                               '%s="%s"' % (attr, id))
+                    content = content.replace("%s='%s'" % (attr, attrValue),
+                                              "%s='%s'" % (attr, id))
                 else:
                     print " * blog.py: failed to upload %s" % pid
         return content
