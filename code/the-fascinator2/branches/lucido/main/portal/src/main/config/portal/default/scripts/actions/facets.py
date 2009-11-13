795bc31e65e2a71ac8c76177d679c288e5930327
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-11-13 04:56:08 +0000

    refs #444 updated backup settings ui

--- code/the-fascinator2/branches/lucido/main/portal/src/main/config/portal/default/scripts/actions/facets.py
+++ code/the-fascinator2/branches/lucido/main/portal/src/main/config/portal/default/scripts/actions/facets.py
@@ -1,4 +1,6 @@
+from au.edu.usq.fascinator.common import JsonConfigHelper
 from au.edu.usq.fascinator.portal import Portal
+from java.util import HashMap
 
 class FacetActions:
     def __init__(self):
@@ -25,6 +27,28 @@ class FacetActions:
                 if deletes[i] == "false":
                     portal.set("portal/facet-fields/%s/label" % field, labels[i])
                     portal.set("portal/facet-fields/%s/display" % field, displays[i])
+            portalManager.save(portal)
+        elif func == "backup-update":
+            portal.set("portal/backup/email", formData.get("email"))
+            pathIds = formData.get("pathIds").split(",")
+            actives = formData.getValues("backup-active")
+            renditions = formData.getValues("backup-rendition")
+            views = formData.getValues("backup-view")
+            paths = HashMap()
+            for pathId in pathIds:
+                path = formData.get("%s-path" % pathId)
+                active = str(pathId in actives).lower()
+                rendition = str(pathId in renditions).lower()
+                view = str(pathId in views).lower()
+                ignoreFilter = formData.get("%s-ignore" % pathId)
+                json = JsonConfigHelper()
+                json.set("active", active)
+                json.set("include-rendition-meta", rendition)
+                json.set("include-portal-view", view)
+                json.set("ignoreFilter", ignoreFilter)
+                paths.put(path, json.getMap("/"))
+            print " *** paths=%s" % paths
+            portal.setMap("portal/backup/paths", paths)
             portalManager.save(portal)
         writer = response.getPrintWriter("text/plain")
         writer.println(result)
