fa83fcd7ccef6d8b7efb381508cc91e6e779d9f4
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-11-25 01:59:52 +0000

    updated with strategic plan facet, some minor wording tweaks

--- code/the-fascinator2/branches/policies2/core/src/main/dist/update-policies.bat
+++ code/the-fascinator2/branches/policies2/core/src/main/dist/update-policies.bat
@@ -1,4 +1,4 @@
 @echo off
-java -Dhttp.proxyHost=proxy.usq.edu.au -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=localhost -cp "C:\Program Files\The Fascinator\lib\*" au.edu.usq.fascinator.HarvestClient "C:\Program Files\The Fascinator\rules\usq-policies.json" > "C:\Program Files\The Fascinator\update.log"
+java -Dhttp.proxyHost=proxy.usq.edu.au -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=localhost -cp "C:\Program Files\The Fascinator\lib\*" au.edu.usq.fascinator.HarvestClient "C:\Program Files\The Fascinator\rules\usq-policies-windows.json" > "C:\Program Files\The Fascinator\update.log"
 echo The process has been logged to C:\Program Files\The Fascinator\update.log
 pause
