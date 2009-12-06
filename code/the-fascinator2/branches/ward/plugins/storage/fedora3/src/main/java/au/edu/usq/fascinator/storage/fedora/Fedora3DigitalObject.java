5999b494a2ae510766583b7c2f4f7d663747e86e
ward <ward@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-12-06 23:26:04 +0000

    merged with the trunk

--- code/the-fascinator2/branches/ward/plugins/storage/fedora3/src/main/java/au/edu/usq/fascinator/storage/fedora/Fedora3DigitalObject.java
+++ code/the-fascinator2/branches/ward/plugins/storage/fedora3/src/main/java/au/edu/usq/fascinator/storage/fedora/Fedora3DigitalObject.java
@@ -26,39 +26,61 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import au.edu.usq.fascinator.api.storage.Payload;
+import au.edu.usq.fascinator.api.storage.PayloadType;
 import au.edu.usq.fascinator.common.storage.impl.GenericDigitalObject;
 import au.edu.usq.fedora.RestClient;
+import au.edu.usq.fedora.types.DatastreamProfile;
 import au.edu.usq.fedora.types.DatastreamType;
 import au.edu.usq.fedora.types.ObjectDatastreamsType;
 
+/**
+ * 
+ * @author Oliver Lucido & Linda Octalina
+ * 
+ */
 public class Fedora3DigitalObject extends GenericDigitalObject {
 
     private Logger log = LoggerFactory.getLogger(Fedora3DigitalObject.class);
 
     private RestClient client;
 
-    public Fedora3DigitalObject(RestClient client, String oid) {
+    private String fedoraId;
+
+    public Fedora3DigitalObject(RestClient client, String fedoraId, String oid) {
         super(oid);
+        this.fedoraId = fedoraId;
         this.client = client;
     }
 
+    @Override
     public List<Payload> getPayloadList() {
         List<Payload> dsList = new ArrayList<Payload>();
         try {
-            ObjectDatastreamsType odt = client.listDatastreams(getId());
-            log.debug("Found {} datastreams for {}", odt.getDatastreams()
-                    .size(), getId());
+            ObjectDatastreamsType odt = client.listDatastreams(fedoraId);
             for (DatastreamType dst : odt.getDatastreams()) {
-                Fedora3Payload ds = new Fedora3Payload(client, getId());
-                ds.setId(dst.getDsid());
-                ds.setLabel(dst.getLabel());
-                ds.setContentType(dst.getMimeType());
-                dsList.add(ds);
+                Fedora3Payload ds = new Fedora3Payload(client, fedoraId, dst
+                        .getDsid());
+
+                DatastreamProfile dsProfile = client.getDatastream(fedoraId,
+                        dst.getDsid());
+                if (dsProfile.getDsAltID() != null) {
+                    // Use the PayloadType and dsId from AltID
+                    // The dsId will be used to search the data straem in fedora
+                    String[] altId = dsProfile.getDsAltID().split(":");
+                    ds.setId(altId[1]);
+                    ds.setLabel(dst.getLabel());
+                    ds.setType(PayloadType.valueOf(altId[0]));
+                    if (ds.getType().equals(PayloadType.Data)) {
+                        // If it's a source, set the source there
+                        setSourceId(dst.getDsid());
+                    }
+                    ds.setContentType(dst.getMimeType());
+                    dsList.add(ds);
+                }
             }
         } catch (IOException ioe) {
-            log.error("Failed to list datastreams for {}", getId());
+            log.error("Failed to list datastreams for {}", fedoraId);
         }
         return dsList;
     }
-
 }
