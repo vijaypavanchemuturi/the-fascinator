fd047811fa6405bbe2c25113150c0b09d44b4119
ward <ward@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2009-12-13 23:38:28 +0000

    Add sword support

--- code/the-fascinator2/branches/ward/portal/src/main/java/au/edu/usq/fascinator/portal/FormData.java
+++ code/the-fascinator2/branches/ward/portal/src/main/java/au/edu/usq/fascinator/portal/FormData.java
@@ -1,26 +1,35 @@
 package au.edu.usq.fascinator.portal;
 
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import org.apache.tapestry5.services.Request;
 
 public class FormData {
 
     private Map<String, List<String>> parameters;
 
+    private HttpServletRequest hsr;
+
     public FormData() {
         parameters = new HashMap<String, List<String>>();
     }
 
     public FormData(Request request) {
+        this(request, null);
+    }
+
+    public FormData(Request request, HttpServletRequest hsr) {
         parameters = new HashMap<String, List<String>>();
         for (String name : request.getParameterNames()) {
             List<String> values = Arrays.asList(request.getParameters(name));
             parameters.put(name, values);
         }
+        this.hsr = hsr;
     }
 
     public String get(String name) {
@@ -30,6 +39,16 @@ public class FormData {
 
     public List<String> getValues(String name) {
         return parameters.get(name);
+    }
+
+    public InputStream getInputStream(){
+        InputStream iStream = null;
+        try {
+            iStream = hsr.getInputStream();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return iStream;
     }
 
     public void clear() {
