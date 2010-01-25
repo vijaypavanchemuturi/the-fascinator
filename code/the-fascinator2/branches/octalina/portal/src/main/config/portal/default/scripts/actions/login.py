f32ef30b56a7bd8dff1e4f93cf9555e7df9b4802
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-01-25 03:58:20 +0000

    re-branching trunk

---
+++ code/the-fascinator2/branches/octalina/portal/src/main/config/portal/default/scripts/actions/login.py
@@ -0,0 +1,21 @@
+from authentication import Authentication
+
+class LoginData:
+
+    def __init__(self):
+        self.authentication = Authentication()
+        self.authentication.session_init()
+
+        if self.authentication.is_logged_in():
+            if self.authentication.is_admin():
+                responseMsg = self.authentication.get_name() + ":admin"
+            else:
+                responseMsg = self.authentication.get_name() + ":notadmin"
+        else:
+            responseMsg = self.authentication.get_error()
+            response.setStatus(500)
+        writer = response.getPrintWriter("text/html")
+        writer.println(responseMsg)
+        writer.close()
+
+scriptObject = LoginData()
