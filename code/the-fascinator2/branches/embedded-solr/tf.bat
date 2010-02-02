d28435a3ec5b04aaa1495a96f17330d7b385e973
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-02-02 06:39:33 +0000

    refs #551 557 updated startup scripts for windows

--- code/the-fascinator2/branches/embedded-solr/tf.bat
+++ code/the-fascinator2/branches/embedded-solr/tf.bat
@@ -6,7 +6,24 @@ REM only usable when installed in development mode
 REM get fascinator home dir
 for %%F in ("%0") do set TF_HOME=%%~dpF
 
+if "%1" == "" goto usage
+if "%1" == "start" goto start
+if "%1" == "stop" goto stop
+
 REM set environment
 call %TF_HOME%\tf_env.bat
 
+:start
 start "The Fascinator" /D%TF_HOME%\portal mvn -P dev jetty:run
+goto end
+
+:stop
+pushd %TF_HOME%\portal
+mvn -P dev jetty:stop
+popd
+goto end
+
+:usage
+echo Usage: %0 start|stop
+
+:end
