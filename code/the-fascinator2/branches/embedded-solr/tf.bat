02178c6e14c4e47a602c314c83db3ad1d2a79607
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-02-03 00:40:58 +0000

    refs #557 updated windows dev scripts for running solr using maven

--- code/the-fascinator2/branches/embedded-solr/tf.bat
+++ code/the-fascinator2/branches/embedded-solr/tf.bat
@@ -7,11 +7,18 @@ REM get fascinator home dir
 for %%F in ("%0") do set TF_HOME=%%~dpF
 
 if "%1" == "" goto usage
+if "%1" == "status" goto status
 if "%1" == "start" goto start
 if "%1" == "stop" goto stop
 
 REM set environment
 call %TF_HOME%\tf_env.bat
+
+:status
+set Cmd=tasklist /fi "WINDOWTITLE eq The Fascinator - mvn  -P dev jetty:run" /fo csv /nh
+for /f "tokens=1*" %%i in ('%Cmd% ^| find "cmd.exe"') do goto running
+echo The Fascinator is STOPPED.
+goto end
 
 :start
 start "The Fascinator" /D%TF_HOME%\portal mvn -P dev jetty:run
@@ -24,6 +31,10 @@ popd
 goto end
 
 :usage
-echo Usage: %0 start|stop
+echo Usage: %0 start^|stop
+goto end
+
+:running
+echo The Fascinator is RUNNING.
 
 :end
--- code/the-fascinator2/branches/embedded-solr/tf_harvest.bat
+++ code/the-fascinator2/branches/embedded-solr/tf_harvest.bat
@@ -1,87 +1,38 @@
-#!/bin/bash
-#
-# this script starts a fascinator harvest using maven
-# only usable when installed in development mode
-#
-for /f "tokens=2*" %%i in ('%Cmd% ^| find "ProxyServer"') do set http_proxy=%%j
+@echo off
+
+REM this script starts a fascinator harvest using maven
+REM only usable when installed in development mode
 
 REM get fascinator home dir
 for %%F in ("%0") do set TF_HOME=%%~dpF
 
+set SAMPLE_DIR=%TF_HOME%core\src\test\resources
 if "%1" == "" goto usage
-if "%1" == "start" goto start
-if "%1" == "stop" goto stop
+set JSON_FILE=%1
 
 REM set environment
 call %TF_HOME%\tf_env.bat
 
-set SAMPLE_DIR=$TF_HOME\core\src\test\resources
-if "%1" == "" then goto usage
+set Cmd=tasklist /fi "windowtitle eq The Fascinator - mvn  -P dev jetty:run" /fo csv /nh
+for /f "tokens=1*" %%i in ('%Cmd% ^| find "cmd.exe"') do goto harvest
+echo Please start The Fascinator before harvesting.
+goto end
 
-
-function copy_sample {
-	if [ ! -f $1.json ]; then
-		cp "$1.json.sample" $SAMPLE_DIR/$1.json
-	fi
-	if [ ! -f $1.py ]; then
-		cp "$1.py.sample" $SAMPLE_DIR/$1.py
-	fi
-}
-
-# setup environment
-. $TF_HOME/tf_env.sh
-
-# get platform
-OS=`uname`
-if [ "$OS" == "Darwin" ]; then
-	NUM_PROCS=`ps a | grep [j]etty | wc -l`
-else
-	NUM_PROCS=`pgrep -l -f jetty | wc -l`
-fi
-if [ $NUM_PROCS == 1 ]; then
-	pushd $TF_HOME/core
-	if [[ $1 == /* ]]; then
-		BASE_FILE=$1
-	else
-		BASE_FILE=$SAMPLE_DIR/$1
-	fi
-	copy_sample $BASE_FILE
-	mvn -P dev -DjsonFile=$BASE_FILE.json exec:java
-	popd
-else
-	echo "Please start The Fascinator before harvesting."
-fi
+:harvest
+if exist "%JSON_FILE%" (set BASE_FILE=%JSON_FILE%) else (set BASE_FILE=%SAMPLE_DIR%\%JSON_FILE%)
+if not exist "%BASE_FILE%.json" copy "%BASE_FILE%.json.sample" "%BASE_FILE%.json"
+if not exist "%BASE_FILE%.py" copy "%BASE_FILE%.py.sample" "%BASE_FILE%.py"
+pushd %TF_HOME%\core
+mvn -P dev -DjsonFile="%BASE_FILE%.json" exec:java
+popd
+goto end
 
 :usage
-echo "Usage: `basename $0` <jsonFile>"
-echo "Where jsonFile is a JSON configuration file"
-echo "If jsonFile is not an absolute path, the file is assumed to be in:"
-echo "    $SAMPLE_DIR"
-echo "Available files:"
-for %%i SAMPLE_FILE in `ls $SAMPLE_DIR/*.json.sample`
-	TMP=${SAMPLE_FILE##*/resources/}
-	echo -n "    "
-	echo $TMP | cut -d . -f 1
-done
+echo Usage: %0 jsonFile
+echo Where jsonFile is a JSON configuration file
+echo If jsonFile is not an absolute path, the file is assumed to be in:
+echo     %SAMPLE_DIR%
+echo Available files:
+for /f "tokens=1,2* delims=." %%i in ('dir /b %SAMPLE_DIR%\*.json.sample') do @echo     %%~ni
 
 :end
-
-
-echo off
-
-REM This script runs a harvest
-
-REM Make sure you check to make sure tf_env.bat reflects
-REM your config.
-
-call tf_env.bat
-
-IF "%1"=="" goto USAGE
-
-start /D%TF_HOME%\code\core mvn -Dhttp.nonProxyHosts=localhost -DXmx1024m -P %1 exec:java
-goto :EOF
-
-:USAGE
-echo Usage: tf_harvest profile
-
-
