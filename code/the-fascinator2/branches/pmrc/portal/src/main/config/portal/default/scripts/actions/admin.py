7e0fb05172e90eba4a9e41093c9a80393f4107a2
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-02-15 04:17:58 +0000

    Branching PMRC

---
+++ code/the-fascinator2/branches/pmrc/portal/src/main/config/portal/default/scripts/actions/admin.py
@@ -0,0 +1,229 @@
+from authentication import Authentication
+
+class LoginData:
+
+    def __init__(self):
+        self.authentication = Authentication()
+        self.authentication.session_init()
+
+        self.writer = response.getPrintWriter("text/html")
+
+        if self.authentication.is_logged_in() and self.authentication.is_admin():
+            self.process()
+        else:
+            self.throw_error("Only administrative users can access this feature")
+
+    def add_user(self):
+        username = formData.get("field")
+        rolename = formData.get("hidden")
+        source = formData.get("source")
+        self.authentication.set_role_plugin(source)
+        self.authentication.set_role(username, rolename)
+
+        err = self.authentication.get_error()
+        if err is None:
+            self.writer.println(username)
+            self.writer.close()
+
+        else:
+            self.throw_error(err)
+
+    def change_password(self):
+        username = formData.get("username")
+        password = formData.get("password")
+        password_confirm = formData.get("password_confirm")
+
+        if password != password_confirm:
+            self.throw_error("The confirm password field does not match the password.")
+
+        else:
+            source = formData.get("source")
+            self.authentication.set_auth_plugin(source)
+            self.authentication.change_password(username, password)
+
+            err = self.authentication.get_error()
+            if err is None:
+                self.writer.println(username)
+                self.writer.close()
+
+            else:
+                self.throw_error(err)
+
+    def create_role(self):
+        rolename = formData.get("field")
+        source   = formData.get("source")
+        self.authentication.set_role_plugin(source)
+        self.authentication.create_role(rolename)
+
+        err = self.authentication.get_error()
+        if err is None:
+            self.writer.println(rolename)
+            self.writer.close()
+
+        else:
+            self.throw_error(err)
+
+    def create_user(self):
+        username = formData.get("username")
+        password = formData.get("password")
+        password_confirm = formData.get("password_confirm")
+
+        if password != password_confirm:
+            self.throw_error("The confirm password field does not match the password.")
+
+        else:
+            source = formData.get("source")
+            self.authentication.set_auth_plugin(source)
+            self.authentication.create_user(username, password)
+
+            err = self.authentication.get_error()
+            if err is None:
+                self.writer.println(username)
+                self.writer.close()
+
+            else:
+                self.throw_error(err)
+
+    def delete_role(self):
+        rolename = formData.get("rolename")
+        source = formData.get("source")
+        self.authentication.set_role_plugin(source)
+        self.authentication.delete_role(rolename)
+
+        err = self.authentication.get_error()
+        if err is None:
+            self.writer.println(rolename)
+            self.writer.close()
+
+        else:
+            self.throw_error(err)
+
+    def delete_user(self):
+        username = formData.get("username")
+        source = formData.get("source")
+        self.authentication.set_auth_plugin(source)
+        self.authentication.delete_user(username)
+
+        err = self.authentication.get_error()
+        if err is None:
+            self.writer.println(username)
+            self.writer.close()
+
+        else:
+            self.throw_error(err)
+
+    def get_current_access(self):
+        record = formData.get("record")
+        roles_list = self.authentication.get_access_roles_list(record)
+
+        err = self.authentication.get_error()
+        if err is None:
+            # We need a JSON string for javascript
+            plugin_strings = []
+            for plugin in roles_list.keys():
+                roles = roles_list[plugin]
+                if len(roles) > 0:
+                    plugin_strings.append("'" + plugin + "' : ['" + "','".join(roles) + "']")
+                else:
+                    plugin_strings.append("'" + plugin + "' : []")
+            responseMessage = "{" + ",".join(plugin_strings) + "}"
+            self.writer.println(responseMessage)
+            self.writer.close()
+
+        else:
+            self.throw_error(err)
+
+    def grant_access(self):
+        record = formData.get("record")
+        role   = formData.get("role")
+        source = formData.get("source")
+        self.authentication.set_access_plugin(source)
+        self.authentication.grant_access(record, role)
+
+        err = self.authentication.get_error()
+        if err is None:
+            self.writer.println(role)
+            self.writer.close()
+            self.reindex_record(record)
+
+        else:
+            self.throw_error(err)
+
+    def list_users(self):
+        rolename = formData.get("rolename")
+        source = formData.get("source")
+        self.authentication.set_auth_plugin(source)
+        user_list = self.authentication.list_users(rolename)
+
+        err = self.authentication.get_error()
+        if err is None:
+            # We need a JSON string for javascript
+            responseMessage = "{['" + "','".join(user_list) + "']}"
+            self.writer.println(responseMessage)
+            self.writer.close()
+
+        else:
+            self.throw_error(err)
+
+    def process(self):
+        action = formData.get("verb")
+
+        switch = {
+            "add-user"           : self.add_user,
+            "create-role"        : self.create_role,
+            "create-user"        : self.create_user,
+            "delete-role"        : self.delete_role,
+            "delete-user"        : self.delete_user,
+            "change-password"    : self.change_password,
+            "get-current-access" : self.get_current_access,
+            "grant-access"       : self.grant_access,
+            "list-users"         : self.list_users,
+            "remove-user"        : self.remove_user,
+            "revoke-access"      : self.revoke_access
+        }
+        switch.get(action, self.unknown_action)()
+
+    def reindex_record(self, recordId):
+        portalManager = Services.getPortalManager()
+        portalManager.reHarvestObject(recordId)
+
+    def remove_user(self):
+        username = formData.get("username")
+        rolename = formData.get("rolename")
+        source = formData.get("source")
+        self.authentication.set_role_plugin(source)
+        self.authentication.remove_role(username, rolename)
+
+        err = self.authentication.get_error()
+        if err is None:
+            self.writer.println(username)
+            self.writer.close()
+
+        else:
+            self.throw_error(err)
+
+    def revoke_access(self):
+        record = formData.get("record")
+        role   = formData.get("role")
+        source = formData.get("source")
+        self.authentication.set_access_plugin(source)
+        self.authentication.revoke_access(record, role)
+
+        err = self.authentication.get_error()
+        if err is None:
+            self.writer.println(role)
+            self.writer.close()
+            self.reindex_record(record)
+
+        else:
+            self.throw_error(err)
+
+    def throw_error(self, message):
+        response.setStatus(500)
+        self.writer.println("Error: " + message)
+        self.writer.close()
+
+    def unknown_action(self):
+        self.throw_error("Unknown action requested - '" + formData.get("verb") + "'")
+
+scriptObject = LoginData()
---
+++ code/the-fascinator2/branches/pmrc/portal/src/main/config/portal/default/scripts/actions/facets.py
@@ -0,0 +1,77 @@
+from au.edu.usq.fascinator.common import JsonConfigHelper
+from au.edu.usq.fascinator.portal import Portal
+from java.util import HashMap
+
+class FacetActions:
+    def __init__(self):
+        print " * facets.py: formData=%s" % formData
+        result = "{}"
+        portalManager = Services.getPortalManager()
+        portal = portalManager.get(portalId)
+        func = formData.get("func")
+        if func == "view-update":
+            portal.setDescription(formData.get("view-description"))
+            portal.setQuery(formData.get("view-query"))
+            portal.setRecordsPerPage(int(formData.get("view-records-per-page")))
+            portal.setFacetCount(int(formData.get("view-facet-count")))
+            portal.setFacetSort(formData.get("view-facet-sort") is not None)
+            portalManager.save(portal)
+        elif func == "facets-update":
+            portal.removePath("portal/facet-fields")
+            fields = formData.getValues("field")
+            labels = formData.getValues("label")
+            displays = formData.getValues("display")
+            deletes = formData.getValues("delete")
+            for i in range(0, len(fields)):
+                field = fields[i]
+                if deletes[i] == "false":
+                    portal.set("portal/facet-fields/%s/label" % field, labels[i])
+                    portal.set("portal/facet-fields/%s/display" % field, displays[i])
+            portalManager.save(portal)
+        elif func == "backup-update":
+            pathIds = formData.get("pathIds").split(",")
+            actives = formData.getValues("backup-active")
+            deletes = formData.getValues("backup-delete")
+            if actives is None:
+                actives = []
+            #renditions = formData.getValues("backup-rendition")
+            #if renditions is None:
+            #    renditions = []
+            views = formData.getValues("backup-view")
+            if views is None:
+                views = []
+            paths = HashMap()
+            for pathId in pathIds: 
+                if deletes is None or pathId not in deletes:
+                    path = formData.get("%s-path" % pathId)
+                    pathName = path.replace("/", "_").replace("${user.home}", "")
+                    active = str(pathId in actives).lower()
+                    #rendition = str(pathId in renditions).lower()
+                    view = str(pathId in views).lower()
+                    ignoreFilter = formData.get("%s-ignore" % pathId)
+                    
+                    json = HashMap()
+                    json.put("path", path)
+                    json.put("active", active)
+                    json.put("include-portal-view", view)
+                    json.put("ignoreFilter", ignoreFilter)
+                    
+                    storage = HashMap()
+                    storage.put("type", "file-system")
+                    
+                    filesystem = HashMap()
+                    filesystem.put("home", path)
+                    filesystem.put("use-link", "false")
+                    storage.put("file-system", filesystem)
+                    
+                    json.put("storage", storage)
+                    paths.put(pathName, json)
+            # reset the path first
+            portal.setMap("portal/backup/paths", HashMap())
+            portal.setMultiMap("portal/backup/paths", paths)
+            portalManager.save(portal)
+        writer = response.getPrintWriter("text/plain")
+        writer.println(result)
+        writer.close()
+
+scriptObject = FacetActions()
