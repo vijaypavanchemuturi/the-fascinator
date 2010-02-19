2bfb56155b1ba825a0c48bf178b7dfbca3b28b64
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-02-19 06:11:12 +0000

    refs #591 image tagging using jquery image annotation

--- code/the-fascinator2/branches/pmrc/portal/src/main/config/portal/default/scripts/actions/anotar.py
+++ code/the-fascinator2/branches/pmrc/portal/src/main/config/portal/default/scripts/actions/anotar.py
@@ -1,15 +1,16 @@
-
+from au.edu.usq.fascinator.api.indexer import SearchRequest
 from au.edu.usq.fascinator.api.storage import PayloadType
 from au.edu.usq.fascinator.common.storage.impl import GenericPayload
 from au.edu.usq.fascinator.common import JsonConfigHelper
 
-from java.io import BufferedReader, ByteArrayInputStream, InputStreamReader
+from java.io import BufferedReader, ByteArrayInputStream, ByteArrayOutputStream, InputStreamReader
 from java.lang import String, StringBuilder
+
+import json, time
 
 class AnotarPayload(GenericPayload):
     def __init__(self, payload, obj):
         if payload is None:
-            #print "****** NEW PAYLOAD CREATION"
             self.setId(self.generate_id(obj))
             self.setLabel("Anotar")
             self.setContentType("application/json")
@@ -20,29 +21,29 @@ class AnotarPayload(GenericPayload):
         self.setType(PayloadType.Annotation)
 
     def add_json(self, json):
+        #print "**** anotar.py : add_json() : adding json : " + json
         jsonObj = JsonConfigHelper(json)
         jsonObj.set("id", self.getId())
-        #print "**** anotar.py : add_json() : new id : " + self.getId()
         rootUri = jsonObj.get("annotates/rootUri")
         if rootUri is not None:
-            myUri = rootUri + "#" + self.getId()
+            baseUrl = "http://%s:%s" % (request.serverName, serverPort)
+            myUri = baseUrl + rootUri + "#" + self.getId()
             jsonObj.set("uri", myUri)
-            #print "**** anotar.py : add_json() : new uri : " + myUri
 
-        self.__content = jsonObj.toString()
+        jsonObj.set("schemaVersionUri", "http://www.purl.org/anotar/schema/0.1")
+
+        self.__content = jsonObj.toString(False)
+        return self.__content
         #print "**** anotar.py : add_json() : completed json : " + self.__content
 
     def generate_id(self, obj):
         counter = 0
         fileName = "anotar." + str(counter)
-        #print "****** TEST NEW ID : " + fileName
         payload = obj.getPayload(fileName)
         while payload is not None:
             counter = counter + 1
             fileName = "anotar." + str(counter)
-            #print "****** TEST NEW ID : " + fileName
             payload = obj.getPayload(fileName)
-        #print "****** NEW ID SELECTED : " + fileName
         return fileName
 
     def get_content(self):
@@ -72,56 +73,124 @@ class AnotarPayload(GenericPayload):
         else:
             return None
 
+    def get_with_type(self, type):
+        myJson = self.get_content()
+        jsonObj = JsonConfigHelper(myJson)
+        myType = jsonObj.get("type")
+        if myType == type:
+            return jsonObj
+        else:
+            return None
+
     def getInputStream(self):
         ourString = String(self.__content)
         return ByteArrayInputStream(ourString.getBytes("UTF-8"))
 
 class AnotarData:
     def __init__(self):
-        action = formData.get("action")
+        self.action = formData.get("action")
         self.rootUri = formData.get("rootUri")
-        annoId = formData.get("id")
-        json = formData.get("json")
-        #print "**** anotar.py : Start : " + action
+        
+        if self.rootUri.find("?ticks") > -1:
+            self.rootUri = self.rootUri[:self.rootUri.find("?ticks")]
+        
+        self.json = formData.get("json")
+        self.type = formData.get("type")
+        #print "**** anotar.py : Action : " + action
 
         self.obj = Services.storage.getObject(self.rootUri)
 
-        if action == "get":
-            # Response is a list of objects (length 1)
-            #print "**** anotar.py : GET : " + annoId
-            result = self.__get(annoId)
-        elif action == "getList":
+        if self.action == "getList":
             # Repsonse is a list of object (nested)
-            #print "**** anotar.py : GET_LIST : " + self.rootUri
-            result = self.__get_list()
-        elif action == "put":
+            #print "**** anotar.py : GET_SOLR : " + self.rootUri
+            result = self.__search_solr()
+        elif self.action == "put":
             # Response is an ID
             #print "**** anotar.py : PUT : " + self.rootUri
-            result = self.__put(json)
+            result = self.__put()
+        elif self.action == "get-image":
+            self.type = "http://www.purl.org/anotar/ns/type/0.1#ImageTag"
+            result = '{"result":' + self.__search_solr() + '}'
+            if result:
+                imageTagList = []
+                imageTags = JsonConfigHelper(result).getJsonList("result")
+                for imageTag in imageTags:
+                    imageAno = JsonConfigHelper()
+                    locatorValue = imageTag.getJsonList("annotates/locators").get(0).get("value")
+                    if locatorValue and locatorValue.find("#xywh=")>-1:
+                        _, locatorValue = locatorValue.split("#xywh=")
+                        left, top, width, height = locatorValue.split(",")
+                        imageAno.set("top", top)
+                        imageAno.set("left", left)
+                        imageAno.set("width", width)
+                        imageAno.set("height", height)
+                        imageAno.set("id", imageTag.get("id"))
+                        imageAno.set("text", imageTag.get("content/literal"))
+                        imageTagList.append(imageAno.toString())
+                result = "[" + ",".join(imageTagList) + "]"
+        elif self.action == "save-image":
+            jsonTemplate = """
+{
+  "clientVersion" : {
+    "literal" : "Annotate Client (0.1)",
+    "uri" : "http://fascinator.usq.edu.au/annotate/client/version#0.1"
+  },
+  "type" : "http://www.purl.org/anotar/ns/type/0.1#ImageTag",
+  "title" : {
+    "literal" : null,
+    "uri" : null
+  },
+  "annotates" : {
+    "uri" : "%s",
+    "rootUri" : "%s",
+    "locators" : [ {
+      "type" : "http://www.w3.org/TR/2009/WD-media-frags-20091217",
+      "value" : "%s"
+    } ]
+  },
+  "creator" : {
+    "literal" : null,
+    "uri" : "http://fascinator.usq.edu.au/trac/wiki/Annotate/schema/0.1/anotar-user-ns#Anonymous",
+    "email" : {
+      "literal" : null
+    }
+  },
+  "dateCreated" : {
+    "literal" : "%s",
+    "uri" : null
+  },
+  "dateModified" : {
+    "literal" : null,
+    "uri" : null
+  },
+  "content" : {
+    "mimeType" : "text/plain",
+    "literal" : "%s",
+    "formData" : {
+    }
+  },
+  "isPrivate" : false,
+  "lang" : "en"
+}
+"""
+            mediaDimension = "xywh=%s,%s,%s,%s" % (formData.get("left"), formData.get("top"), formData.get("width"), formData.get("height"))
+            locatorValue = "%s#%s" % (self.rootUri, mediaDimension)
+            dateCreated = time.strftime("%Y-%m-%dT%H:%M:%SZ")
+            self.json = jsonTemplate % (self.rootUri, self.rootUri, locatorValue, dateCreated, formData.get("text"))
+            result = self.__put()
         writer = response.getPrintWriter("text/plain")
         writer.println(result)
         writer.close()
 
-    def __put(self, json):
+    def __put(self):
         #print "**** anotar.py : __put() : start"
         p = self.__create_payload()
         #print "**** anotar.py : __put() : adding jason : " + json
-        p.add_json(json);
+        json = p.add_json(self.json)
         Services.storage.addPayload(self.rootUri, p)
-        Services.indexer.index(self.rootUri, p.getId())
+        Services.indexer.annotate(self.rootUri, p.getId())
         #print "**** anotar.py : __put() : ID : " + p.getId()
-        return '{"id": "' + p.getId() + '", "ok": true}'
-
-    def __get(self, annoId):
-        #print "**** anotar.py : __get() : start"
-        p = self.__get_payload(annoId)
-        response = p.get_content();
-        if response is not None:
-            #print "**** anotar.py : __get() : " + response
-            response = "[" + response + "]"
-        else:
-            response = "[]"
-        return response
+        return json
 
     def __get_list(self):
         topLevel = "["
@@ -141,15 +210,6 @@ class AnotarData:
         #print "**** anotar.py : __create_payload() : start"
         return AnotarPayload(None, self.obj)
 
-    def __get_payload(self, annoId):
-        #print "**** anotar.py : __get_payload() : start"
-        payload = self.obj.getPayload(annoId);
-        if payload is not None:
-            #print "**** anotar.py : __get_payload() : found"
-            return AnotarPayload(payload, self.obj)
-        else:
-            return None
-
     def __get_payloads(self):
         #print "**** anotar.py : __get_payloads() : start : " + self.rootUri
         payloads = self.obj.getPayloadList()
@@ -158,5 +218,47 @@ class AnotarData:
             return_list.append(AnotarPayload(payload, self.obj))
         return return_list
 
+    def __search_solr(self):
+        query = "(rootUri:\"" + self.rootUri + "\""
+        query += " AND type:\"" + self.type + "\")"
+
+        req = SearchRequest(query)
+        req.setParam("facet", "false")
+        req.setParam("rows", str(99999))
+        req.setParam("sort", "dateCreated asc")
+        req.setParam("start", str(0))
+
+        #security_roles = page.authentication.get_roles_list();
+        #security_query = 'security_filter:("' + '" OR "'.join(security_roles) + '")'
+        #req.addParam("fq", security_query)
+
+        out = ByteArrayOutputStream()
+        Services.indexer.annotateSearch(req, out)
+        result = JsonConfigHelper(ByteArrayInputStream(out.toByteArray()))
+
+        # Every annotation for this URI
+        result = result.getJsonList("response/docs")
+        docs = []
+        rootDocs = []
+        docsDict = {}
+        # Build a dictionary of the annotations
+        for doc in result:
+            jsonStr = unicode(doc.get("jsonString")).encode("utf-8")
+            doc = json.read(jsonStr)
+            doc["replies"] = []
+            docs.append(doc)
+            docsDict[doc["uri"]] = doc
+            if doc["annotates"]["uri"]==doc["annotates"]["rootUri"]:
+                rootDocs.append(doc)
+
+        # Now process the dictionary
+        for doc in docs:
+            # If we are NOT a top level annotation
+            if doc["annotates"]["uri"]!=doc["annotates"]["rootUri"]:
+                # Find what we are annotating
+                d = docsDict[doc["annotates"]["uri"]]
+                d["replies"].append(doc)    # Add ourselves to its reply list
+
+        return json.write(rootDocs)
 
 scriptObject = AnotarData()
