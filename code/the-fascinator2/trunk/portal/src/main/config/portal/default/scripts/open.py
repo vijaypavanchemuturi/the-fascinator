3b67769e00422640f3b23d623fc4fb6576d31add
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-04-29 01:16:28 +0000

    fixes #638 open file uses the file.path object property now instead of object id

--- code/the-fascinator2/trunk/portal/src/main/config/portal/default/scripts/open.py
+++ code/the-fascinator2/trunk/portal/src/main/config/portal/default/scripts/open.py
@@ -1,14 +1,21 @@
 from java.awt import Desktop
 from java.io import File
+from java.lang import Exception
 
 class OpenData:
     def __init__(self):
-        if formData.get("func") == "open-file":
-            file = formData.get("file")
-            print " * open.py: Opening file %s..." % file
-            Desktop.getDesktop().open(File(file))
         writer = response.getPrintWriter("text/plain; charset=UTF-8")
-        writer.println("{}")
+        jsonResponse = "{}"
+        try:
+            oid = formData.get("oid")
+            object = Services.getStorage().getObject(oid);
+            filePath = object.getMetadata().getProperty("file.path")
+            object.close()
+            print "Opening file '%s'..." % filePath
+            Desktop.getDesktop().open(File(filePath))
+        except Exception, e:
+            jsonResponse = '{ "message": "%s" }' % e.getMessage()
+        writer.println(jsonResponse)
         writer.close()
 
 scriptObject = OpenData()
