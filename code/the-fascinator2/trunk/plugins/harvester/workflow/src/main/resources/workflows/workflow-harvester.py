b8e3b227ba06a640044bb5e3851cd585a7d50f2d
pendlebu <pendlebu@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-05-20 06:20:35 +0000

    refs #743. FFmpeg metadata is now indexed if the 'ffmpeg.info' payload exists. Best results come from FFmpeg builds with access to FFprobe.

--- code/the-fascinator2/trunk/plugins/harvester/workflow/src/main/resources/workflows/workflow-harvester.py
+++ code/the-fascinator2/trunk/plugins/harvester/workflow/src/main/resources/workflows/workflow-harvester.py
@@ -1,11 +1,13 @@
-from java.io import ByteArrayInputStream
-from java.lang import String
-import md5, os, time
+import time
 
 from au.edu.usq.fascinator.api.storage import StorageException
 from au.edu.usq.fascinator.common import JsonConfigHelper
 from au.edu.usq.fascinator.common.storage import StorageUtils
 from au.edu.usq.fascinator.indexer.rules import AddField, New
+
+from java.io import ByteArrayInputStream
+from java.lang import String
+
 #
 # Available objects:
 #    indexer    : Indexer instance
@@ -202,7 +204,83 @@ if pid == metaPid:
     except StorageException, e:
         #print "Failed to index aperture data (%s)" % str(e)
         pass
-    
+
+    ### Check if ffmpeg.info exists or not
+    try:
+        ffmpegPayload = object.getPayload("ffmpeg.info")
+        ffmpeg = pyUtils.getJsonObject(ffmpegPayload.open())
+        ffmpegPayload.close()
+        if ffmpeg is not None:
+            # Dimensions
+            width = ffmpeg.get("video/width")
+            height = ffmpeg.get("video/height")
+            if width is not None and height is not None:
+                rules.add(AddField("dc_size", width + " x " + height))
+
+            # Duration
+            duration = ffmpeg.get("duration")
+            if duration is not None and int(duration) > 0:
+                if int(duration) > 59:
+                    secs = int(duration) % 60
+                    mins = (int(duration) - secs) / 60
+                    rules.add(AddField("dc_duration", "%dm %ds" % (mins, secs)))
+                else:
+                    rules.add(AddField("dc_duration", duration + " second(s)"))
+
+            # Format
+            media = ffmpeg.get("format/label")
+            if media is not None:
+                rules.add(AddField("dc_media_format", media))
+
+            # Video
+            codec = ffmpeg.get("video/codec/simple")
+            label = ffmpeg.get("video/codec/label")
+            if codec is not None and label is not None:
+                rules.add(AddField("video_codec_simple", codec))
+                rules.add(AddField("video_codec_label", label))
+                rules.add(AddField("meta_video_codec", label + " (" + codec + ")"))
+            else:
+                if codec is not None:
+                    rules.add(AddField("video_codec_simple", codec))
+                    rules.add(AddField("meta_video_codec", codec))
+                if label is not None:
+                    rules.add(AddField("video_codec_label", label))
+                    rules.add(AddField("meta_video_codec", label))
+            pixel_format = ffmpeg.get("video/pixel_format")
+            if pixel_format is not None:
+                rules.add(AddField("meta_video_pixel_format", pixel_format))
+
+            # Audio
+            codec = ffmpeg.get("audio/codec/simple")
+            label = ffmpeg.get("audio/codec/label")
+            if codec is not None and label is not None:
+                rules.add(AddField("audio_codec_simple", codec))
+                rules.add(AddField("audio_codec_label", label))
+                rules.add(AddField("meta_audio_codec", label + " (" + codec + ")"))
+            else:
+                if codec is not None:
+                    rules.add(AddField("audio_codec_simple", codec))
+                    rules.add(AddField("meta_audio_codec", codec))
+                if label is not None:
+                    rules.add(AddField("audio_codec_label", label))
+                    rules.add(AddField("meta_audio_codec", label))
+            sample_rate = ffmpeg.get("audio/sample_rate")
+            if sample_rate is not None:
+                sample_rate = "%.1f KHz" % (int(sample_rate) / 1000)
+            channels = ffmpeg.get("audio/channels")
+            if channels is not None:
+                channels += " Channel(s)"
+            if sample_rate is not None and channels is not None:
+                rules.add(AddField("meta_audio_details", sample_rate + ", " + channels))
+            else:
+                if sample_rate is not None:
+                    rules.add(AddField("meta_audio_details", sample_rate))
+                if channels is not None:
+                    rules.add(AddField("meta_audio_details", channels))
+    except StorageException, e:
+        #print "Failed to index FFmpeg metadata (%s)" % str(e)
+        pass
+
     # Workflow data
     WORKFLOW_ID = "workflow1"
     wfChanged = False
