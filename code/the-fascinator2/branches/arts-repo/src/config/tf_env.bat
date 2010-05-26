53adee82ad977e25aa2922c0535ec87a7fca33d8
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-05-26 01:03:30 +0000

    refs #757 updated pom

--- code/the-fascinator2/branches/arts-repo/src/config/tf_env.bat
+++ code/the-fascinator2/branches/arts-repo/src/config/tf_env.bat
@@ -1,18 +1,16 @@
 @echo off
-
 REM this script sets the environment for the fascinator scripts
 
 REM find java installation
 if not defined JAVA_HOME (
-  echo Detecting Java...
   set KeyName=HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit
-  set Cmd=reg query "!KeyName!" /s /v JavaHome
+  set Cmd=reg query "!KeyName!" /s
   for /f "tokens=2*" %%i in ('%Cmd% ^| findstr "JavaHome" 2^> NUL') do set JAVA_HOME=%%j
 )
 
 REM find proxy server
 set KeyName=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings
-set Cmd=reg query "%KeyName%" /s /v ProxyServer
+set Cmd=reg query "%KeyName%" /s
 for /f "tokens=2*" %%i in ('%Cmd% ^| findstr "ProxyServer" 2^> NUL') do set http_proxy=%%j
 for /f "tokens=1,2 delims=:" %%i in ("%http_proxy%") do (
   set PROXY_HOST=%%i
@@ -20,6 +18,6 @@ for /f "tokens=1,2 delims=:" %%i in ("%http_proxy%") do (
 )
 
 REM set environment
-if not defined FASCINATOR_HOME set FASCINATOR_HOME=%USERPROFILE%\.fascinator
-if not defined SOLR_HOME set SOLR_HOME=%FASCINATOR_HOME%\solr
-set JAVA_OPTS=-XX:MaxPermSize=128m -Xmx512m -Dhttp.proxyHost=%PROXY_HOST% -Dhttp.proxyPort=%PROXY_PORT% -Dhttp.nonProxyHosts=localhost -Dfascinator.home="%FASCINATOR_HOME%" -Dsolr.solr.home="%SOLR_HOME%" %JAVA_OPTS%
+set FASCINATOR_HOME=%PROGRAM_DIR%\home
+set SOLR_HOME=%FASCINATOR_HOME%\solr
+set JAVA_OPTS=-XX:MaxPermSize=128m -Xmx512m -Dhttp.proxyHost=%PROXY_HOST% -Dhttp.proxyPort=%PROXY_PORT% -Dhttp.nonProxyHosts=localhost -Dfascinator.home="%FASCINATOR_HOME%" -Dsolr.solr.home="%SOLR_HOME%"
--- code/the-fascinator2/branches/arts-repo/src/config/tf_start.bat
+++ code/the-fascinator2/branches/arts-repo/src/config/tf_start.bat
@@ -1,17 +1,9 @@
 @echo off
-
 setlocal ENABLEDELAYEDEXPANSION
 
-call "%TF_HOME%tf_env.bat"
+set PROGRAM_DIR=%~dp0
+call "%PROGRAM_DIR%tf_env.bat"
 
-REM copy solr configuration if missing
-if exist %SOLR_HOME% goto start
-
-:copy
-mkdir %SOLR_HOME%
-xcopy /q /s /y solr %SOLR_HOME%
-
-:start
 pushd jetty
 java -DSTART=start.config %JAVA_OPTS% -jar start.jar etc/jetty.xml
 popd
