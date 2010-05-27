20fd3bba8b75ee9f32133fda120584b0af38a51f
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-05-27 06:47:00 +0000

    refs #819 created workflow configuration placeholder templates

---
+++ code/the-fascinator2/branches/arts-repo/src/config/home/portal/default/arts/scripts/workflows/arts-basic-reviewing.py
@@ -0,0 +1,12 @@
+class ArtsBasicReviewing:
+    def __init__(self):
+        pass
+
+    def getFormData(self, field):
+        value = formData.get(field)
+        if value is None:
+            return ""
+        else:
+            return value
+    
+scriptObject = ArtsBasicReviewing()
