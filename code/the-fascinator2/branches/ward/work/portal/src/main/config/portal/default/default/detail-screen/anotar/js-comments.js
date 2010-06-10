14523a9c612e794b2ade1382211f0240a4ad40f9
ward <ward@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-06-10 04:10:06 +0000

    Making a new working branch

---
+++ code/the-fascinator2/branches/ward/work/portal/src/main/config/portal/default/default/detail-screen/anotar/js-comments.js
@@ -0,0 +1,18 @@
+#if(!$self.mimeType.startsWith("audio/") && !$self.mimeType.startsWith("video/"))
+<script type="text/javascript">
+$(function() {
+    commentConfig = {
+        pageUri: "$oid",
+        #if("$!creator" != "")creator: "$creator",#end
+        #if("$!creatorUri" != "")creatorUri: "$creatorUri",#end
+        tagList: "p, h1, h2, h3, h4, h5, h6, .annotatable",
+        interfaceLabel: " <img src='$portalPath/images/icons/comment_add.png' title='Add new comment' /> Comments:",
+        replyLabel: " <img src='$portalPath/images/icons/comments_add.png' title='Reply to this comment' />",
+        interfaceVisible: $self.mimeType.startsWith("image/").toString().toLowerCase(),
+        loadAnnotations: function() { loadAnnotations(this, "$oid") },
+        submitAnnotation: function(data, annotateDiv) { submitAnnotation(this, "$oid", data, annotateDiv) }
+    }
+    anotarFactory(jQuery, commentConfig);
+});
+</script>
+#end
