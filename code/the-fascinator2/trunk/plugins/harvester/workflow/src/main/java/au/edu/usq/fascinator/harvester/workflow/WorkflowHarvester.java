2726bffeb93d6b3f63fdfd349fd4c01fa207f301
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-07-01 01:05:12 +0000

    improving performance of workflow harvester to support watcher queue

--- code/the-fascinator2/trunk/plugins/harvester/workflow/src/main/java/au/edu/usq/fascinator/harvester/workflow/WorkflowHarvester.java
+++ code/the-fascinator2/trunk/plugins/harvester/workflow/src/main/java/au/edu/usq/fascinator/harvester/workflow/WorkflowHarvester.java
@@ -21,20 +21,27 @@ package au.edu.usq.fascinator.harvester.workflow;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import au.edu.usq.fascinator.api.harvester.HarvesterException;
 import au.edu.usq.fascinator.api.storage.DigitalObject;
 import au.edu.usq.fascinator.api.storage.StorageException;
+import au.edu.usq.fascinator.common.JsonConfigHelper;
 import au.edu.usq.fascinator.common.harvester.impl.GenericHarvester;
 import au.edu.usq.fascinator.common.storage.StorageUtils;
 
@@ -55,6 +62,9 @@ public class WorkflowHarvester extends GenericHarvester {
 
     /** flag for forcing local update */
     private boolean forceUpdate;
+    
+    /** Render chains */
+    private Map<String, Map<String, List<String>>> renderChains;
 
     public WorkflowHarvester() {
         super("workflow-harvester", "Workflow Harvester");
@@ -66,6 +76,20 @@ public class WorkflowHarvester extends GenericHarvester {
                 "harvester/workflow-harvester/force-storage", "true"));
         forceUpdate = Boolean.parseBoolean(getJsonConfig().get(
                 "harvester/workflow-harvester/force-update", "false"));
+        
+        // Order is significant
+        renderChains = new LinkedHashMap();
+        Map<String, JsonConfigHelper> renderTypes = getJsonConfig()
+                .getJsonMap("renderTypes");
+        for (String name : renderTypes.keySet()) {
+            Map<String, List<String>> details = new HashMap();
+            JsonConfigHelper chain = renderTypes.get(name);
+            details.put("fileTypes", getList(chain, "fileTypes"));
+            details.put("harvestQueue", getList(chain, "harvestQueue"));
+            details.put("indexOnHarvest", getList(chain, "indexOnHarvest"));
+            details.put("renderQueue", getList(chain, "renderQueue"));
+            renderChains.put(name, details);
+        }
     }
 
     @Override
@@ -117,7 +141,57 @@ public class WorkflowHarvester extends GenericHarvester {
         props.setProperty("file.path", FilenameUtils.separatorsToUnix(file
                 .getAbsolutePath()));
         objectId = object.getId();
+        
+     // Store rendition information if we have it
+        String ext = FilenameUtils.getExtension(file.getName());
+        for (String chain : renderChains.keySet()) {
+            Map<String, List<String>> details = renderChains.get(chain);
+            if (details.get("fileTypes").contains(ext)) {
+                storeList(props, details, "harvestQueue");
+                storeList(props, details, "indexOnHarvest");
+                storeList(props, details, "renderQueue");
+            }
+        }
+        
         object.close();
         return objectId;
+    }
+    
+    /**
+     * Get a list of strings from configuration
+     * 
+     * @param json Configuration object to retrieve from
+     * @param field The path to the list
+     * @return List<String> The resulting list
+     */
+    private List<String> getList(JsonConfigHelper json, String field) {
+        List<String> result = new ArrayList();
+        List<Object> list = json.getList(field);
+        for (Object object : list) {
+            result.add((String) object);
+        }
+        return result;
+    }
+    
+    /**
+     * Take a list of strings from a Java Map, concatenate the values together
+     * and store them in a Properties object using the Map's original key.
+     * 
+     * @param props Properties object to store into
+     * @param details The full Java Map
+     * @param field The key to use in both objects
+     */
+    private void storeList(Properties props, Map<String, List<String>> details,
+            String field) {
+        Set<String> valueSet = new LinkedHashSet<String>();
+        // merge with original property value if exists
+        String currentValue = props.getProperty(field, "");
+        if (!"".equals(currentValue)) {
+            String[] currentList = currentValue.split(",");
+            valueSet.addAll(Arrays.asList(currentList));
+        }
+        valueSet.addAll(details.get(field));
+        String joinedList = StringUtils.join(valueSet, ",");
+        props.setProperty(field, joinedList);
     }
 }
