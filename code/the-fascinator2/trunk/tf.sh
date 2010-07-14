c3c52d8e83c974050c8f081a1747263042e19d32
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-07-14 04:39:21 +0000

    updated linux/macosx startup scripts to use PID instead of process names

--- code/the-fascinator2/trunk/tf.sh
+++ code/the-fascinator2/trunk/tf.sh
@@ -3,78 +3,128 @@
 # this script controls the fascinator using maven and jetty
 # only usable when installed in development mode
 #
+# get absolute path of where the script is run from
+PROG_DIR=`cd \`dirname $0\`; pwd`
 
-# suppress console output from pushd/popd
-pushd() {
-	builtin pushd "$@" > /dev/null
+# file to store pid
+PID_FILE=$PROG_DIR/tf.pid
+
+# display program header
+echo "The Fascinator (Developer)"
+
+usage() {
+	echo "Usage: `basename $0` {start|stop|restart|status|build|rebuild}"
+	exit 1
 }
-popd() {
-	builtin popd "$@" > /dev/null
+
+running() {
+	[ -f $PID_FILE ] || return 1
+	PID=$(cat $PID_FILE)
+	ps -p $PID > /dev/null 2> /dev/null || return 1
+	return 0
 }
 
-# get fascinator home dir
-pushd `dirname $0`
-TF_HOME=`pwd`
-popd
+# check script arguments
+[ $# -gt 0 ] || usage
 
-if [ "$1" == "" ]; then
-	echo "Usage: `basename $0` start|stop|restart|status|build|rebuild"
-	exit 0
-fi
+# configure environment
+. $PROG_DIR/tf_env.sh
 
-# setup environment
-. $TF_HOME/tf_env.sh
+# perform action
+ACTION="$1"
+shift
+ARGS="$*"
 
-# get platform
-OS=`uname`
-if [ "$OS" == "Darwin" ]; then
-	NUM_PROCS=`ps a | grep [j]etty | wc -l`
-else
-	NUM_PROCS=`pgrep -l -f jetty | wc -l`
-fi
-
-if [ "$1" == "status" ]; then
-	if [ $NUM_PROCS == 1 ]; then
-		echo "The Fascinator is RUNNING."
-	else
-		echo "The Fascinator is STOPPED."
-	fi
-elif [ "$1" == "stop" -o "$1" == "restart" ]; then
-	if [ $NUM_PROCS == 1 ]; then
-		echo "Stopping The Fascinator..."
-		pushd $TF_HOME/portal
-		mvn -P dev jetty:stop
-		popd
-	else
-		echo "The Fascinator is already STOPPED."
-	fi
-fi
-
-if [ "$1" == "start" -o "$1" == "restart" ]; then
-	if [ $NUM_PROCS == 1 ]; then
-		echo "The Fascinator is already RUNNING."
-	else
-		echo "Starting The Fascinator..."
-		pushd $TF_HOME/portal
-		mkdir -p $FASCINATOR_HOME/logs/
-		nohup mvn -P dev jetty:run &> $FASCINATOR_HOME/logs/stdout.out &
-		popd
-	fi
-	echo "Application logs: $FASCINATOR_HOME/logs"
-	echo "Standard out log: $FASCINATOR_HOME/log/stdout.out"
-fi
-
-if [ "$1" == "build" -o "$1" == "rebuild" ]; then
-	if [ $NUM_PROCS == 1 ]; then
-		echo "The Fascinator is currently RUNNING."
-		echo "Please stop it before attempting a $1."
-	else
-		echo "Building The Fascinator..."
-		if [ "$1" == "build" ]; then
-			mvn install
+start() {
+	echo " * Starting The Fascinator..."
+	if [ -f $PID_FILE ]; then
+		echo "   - Found PID file: $PID_FILE"
+		PID=`cat $PID_FILE`
+		if [ -z "`ps -p $PID -o pid=`" ]; then
+			echo "   - Process $PID not found! Removing PID file..."
+			rm -f $PID_FILE
 		else
-			mvn clean install
+			echo "   - Already running!"
+			exit 0
 		fi
 	fi
-fi
+	mkdir -p $TF_HOME/logs
+	cd $PROG_DIR/portal
+	nohup mvn $ARGS -P dev jetty:run &> $TF_HOME/logs/stdout.out &
+	cd - > /dev/null 2> /dev/null
+	echo $! > $PID_FILE
+	echo "   - Started on `date`"
+	echo "   - PID: `cat $PID_FILE`"
+	echo "   - Log files in $TF_HOME/logs"
+}
 
+stop() {
+	echo " * Stopping The Fascinator..."
+	if [ -f $PID_FILE ]; then
+		echo "   - Found PID file: $PID_FILE"
+		PID=`cat $PID_FILE`
+		if [ -z "`ps -p $PID -o pid=`" ]; then
+			echo "   - Process $PID not found! Removing PID file..."
+		else
+			kill $PID
+			sleep 5
+			if running; then
+				echo "   - After 5s, process still running, waiting 15s..."
+				sleep 15
+				if running; then
+					echo "   - After 15s, process still running, forcing shutdown..."
+					kill -9 $PID
+				fi
+			fi
+			echo "   - Stopped on `date`"
+		fi
+		rm -f $PID_FILE
+	else
+		echo "   - Not running!"
+	fi
+}
+
+case "$ACTION" in
+start)
+	start
+	;;
+stop)
+	stop
+	;;
+restart)
+	echo " * Restarting The Fascinator..."
+	if running; then
+		stop
+		sleep 5
+	fi
+	start
+	;;
+build)
+	echo " * Building The Fascinator..."
+	if running; then
+		echo "   - Warning: The Fascinator is running, building may cause unexpected behaviour..."
+	fi
+	mvn $ARGS install
+	;;
+rebuild)
+	echo " * Rebuilding The Fascinator..."
+	if running; then
+		echo "   - Warning: The Fascinator is running, rebuilding may cause unexpected behaviour..."
+	fi
+	mvn $ARGS clean install
+	;;
+status)
+	echo -n " * The Fascinator is "
+	if running; then
+		echo "running"
+		echo "   - PID: `cat $PID_FILE`"
+		echo "   - Log files in $TF_HOME/logs"
+	else
+		echo "not running"
+	fi
+	;;
+*)
+	echo " * Unknown action: $ACTION"
+	usage
+	;;
+esac
