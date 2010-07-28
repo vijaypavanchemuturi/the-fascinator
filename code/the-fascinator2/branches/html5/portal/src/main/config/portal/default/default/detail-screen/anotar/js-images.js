9db04811345d011ae192941e53fa9e5c61b931b1
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-07-28 03:59:05 +0000

    branch for html5 innovation

---
+++ code/the-fascinator2/branches/html5/portal/src/main/config/portal/default/default/detail-screen/anotar/js-images.js
@@ -0,0 +1,23 @@
+<script type="text/javascript" src="$portalPath/js/anotar/image.annotate.js"></script>
+<script type="text/javascript">
+function setupImageTags(selector) {
+    var hash = location.hash.substring(1);
+    #set($dq = '"')
+    #if($isPackage)
+        #set($rootUri = "$dq$oid#$dq+hash")
+    #else
+        #set($rootUri = "$dq$oid$dq")
+    #end
+    if (hash != "blank") {
+        var baseUrl = "$portalPath/actions/anotar.ajax?rootUri=" + $rootUri + "&action=";
+        $(selector).annotateImage({
+            editable: true,
+            saveUrl: baseUrl + "save-image&creator=$!creator&creatorUri=$!creatorUri",
+            getUrl: baseUrl + "get-image",
+            deleteUrl: baseUrl + "delete-image",
+            deletable: $page.authentication.is_admin().toString().toLowerCase()
+        });
+    }
+}
+#if(!$isPackage)$(window).load(function() { setupImageTags("#image-content"); });#end
+</script>
