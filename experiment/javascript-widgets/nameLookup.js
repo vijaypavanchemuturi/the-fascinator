c6c6ad05288562d9a6785d6197055e83cb8c876b
ward <ward@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-08-16 21:00:33 +0000

    Refs #1062 Add name lookup widget

---
+++ experiment/javascript-widgets/nameLookup.js
@@ -0,0 +1,117 @@
+
+(function(){
+  $(function(){
+    function debug(msg){
+      $("#debugMsg").text(msg.toString());
+    }
+    $("#refresh").click(function(){
+      location.href = location.href;
+    });
+    debug("Reloaded ok");
+
+    var dialog=$("<div>Test dialog message!</div>").dialog({
+                  title:"Name lookup", 
+                  height:"auto",
+                  width:300,
+                  modal:false,
+                  resizable:true,
+                  position:"center",
+                  draggable:false,
+                  //buttons:{"OK":function(){$(this).dialog("close");}},
+                  autoOpen:false
+                });
+    var detailDialog=$("<div/>").dialog({
+                  title:"Detail information", 
+                  height:"auto",
+                  width:300,
+                  modal:false,
+                  resizable:true,
+                  position:"center",
+                  draggable:false,
+                  //buttons:{"OK":function(){$(this).dialog("close");}},
+                  autoOpen:false
+    });
+
+    var nameLookupUrl = $("#nameLookup-url").val() || "nameLookup.json";
+    var nameLookup = $(".nameLookup");
+    nameLookup.change(function(){
+      debug("change ok");
+    });
+    nameLookup.keypress(function(){
+      debug("keypress ");
+      var e=$(this);
+      setTimeout(function(){checkNameLookup(e)}, 10);
+    });
+    nameLookup.each(function(c, e){
+      e = $(e);
+      var s = e.next(".nameLookup-link");
+      e.after(s);
+      checkNameLookup(e);
+      s.click(function(){
+        doNameLookup(e.val(), s.position());
+        return false;
+      });
+    });
+    function checkNameLookup(e){
+      $.trim(e.val())===""?e.next().hide():e.next().show();
+    }
+
+    function doNameLookup(queryStr, position){
+      var displayDetails;
+      $.getJSON(nameLookupUrl, {query:queryStr}, function(data){
+        var div, d, a;
+        //alert(data.toSource());
+        div = $("<div/>");
+        div.text("Search result for '" + queryStr + "'");
+        div.append("<hr/>");
+        $.each(data.items, function(c, i){
+          var r;
+          d = $("<div/>");
+          r = $("<input type='radio' name='name' value='"+i.uri+"'/>");
+          d.append(r);
+          d.append(i.title);
+          a = $("<a style='float:right;' href='#'>details</a>");
+          d.append(a);
+          div.append(d);
+          a.click(function(){
+            var pos = $(this).position();
+            pos.left += position.left+70;
+            pos.top += position.top;
+            displayDetails(i.title, i.details, pos, r);
+            return false;
+          });
+        });
+        
+        dialog.html(div);
+        dialog.dialog({position:position, buttons:{
+                        "OK":function(){
+                            alert(div.find("input[name=name]:checked").val());
+                            dialog.dialog("close");
+                          },
+                          "Cancel":function(){
+                            dialog.dialog("close");
+                          }
+                        },
+                      }).dialog("open");
+      });
+      displayDetails = function(name, details, position, link){
+        detailDialog.text(name);
+        detailDialog.append("<hr/>");
+        $.each(details, function(k, v){
+          var d=$("<div/>");
+          d.html("<span>"+k.replace(/^./, function(m){return m.toUpperCase();})+":&#160;</span>");
+          d.append(v);
+          detailDialog.append(d);
+        });
+        detailDialog.dialog({position:position, buttons:{
+                                "OK": function(){link.click(); detailDialog.dialog("close");},
+                                "Cancel": function(){detailDialog.dialog("close");}
+                              }
+                            }).dialog("open").dialog("moveToTop");
+      };
+    }
+  });
+})($);
+
+
+
