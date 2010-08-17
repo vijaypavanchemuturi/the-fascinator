a4e33ceebbeda05179f43d1fa174689943ac9423
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-08-17 21:35:54 +0000

    branching for display screen refactor

---
+++ code/the-fascinator2/branches/display/portal/src/main/java/au/edu/usq/fascinator/portal/services/PortalManager.java
@@ -0,0 +1,55 @@
+/* 
+ * The Fascinator - Portal
+ * Copyright (C) 2008-2009 University of Southern Queensland
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package au.edu.usq.fascinator.portal.services;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+
+import au.edu.usq.fascinator.portal.Portal;
+
+public interface PortalManager {
+
+    public static final String DEFAULT_PORTAL_NAME = "default";
+
+    public static final String DEFAULT_PORTAL_HOME_DIR = "/opt/the-fascinator/config/portal";
+
+    public static final String DEFAULT_PORTAL_HOME_DIR_DEV = "src/main/config/portal";
+
+    public Map<String, Portal> getPortals();
+
+    public Portal getDefault();
+
+    public File getHomeDir();
+
+    public Portal get(String name);
+
+    public boolean exists(String name);
+
+    public void add(Portal portal);
+
+    public void remove(String name);
+
+    public void save(Portal portal);
+
+    public void reharvest(String objectId);
+
+    public void reharvest(Set<String> objectIds);
+
+}
---
+++ code/the-fascinator2/branches/display/vocabulary/src/main/java/au/edu/usq/fascinator/vocabulary/FOAF.java
@@ -0,0 +1,14 @@
+package au.edu.usq.fascinator.vocabulary;
+
+import org.ontoware.rdf2go.model.node.URI;
+import org.ontoware.rdf2go.model.node.impl.URIImpl;
+
+/**
+ * Vocabulary File. Created by org.ontoware.rdf2go.util.VocabularyWriter on Wed Aug 11 14:52:35 EST 2010
+ * input file: C:\Users\dickinso.USQ\AppData\Local\Temp\FOAF.xml
+ * namespace: http://xmlns.com/foaf/spec/
+ */
+public interface FOAF {
+	public static final URI NS_FOAF = new URIImpl("http://xmlns.com/foaf/spec/",false);
+
+}
