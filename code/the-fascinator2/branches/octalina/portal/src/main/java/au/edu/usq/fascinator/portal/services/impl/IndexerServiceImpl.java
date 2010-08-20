816d9f22d16490d5cfa64f07dd3efcb69d72309e
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-08-20 03:41:25 +0000

    refs #1084 #1087 #1083 created subscriber plugin api, event log subscriber and new solr schema for event log

--- code/the-fascinator2/branches/octalina/portal/src/main/java/au/edu/usq/fascinator/portal/services/impl/IndexerServiceImpl.java
+++ code/the-fascinator2/branches/octalina/portal/src/main/java/au/edu/usq/fascinator/portal/services/impl/IndexerServiceImpl.java
@@ -20,7 +20,17 @@ package au.edu.usq.fascinator.portal.services.impl;
 
 import java.io.File;
 import java.io.OutputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import javax.jms.JMSException;
+
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ApplicationStateManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.edu.usq.fascinator.MessagingServices;
 import au.edu.usq.fascinator.api.PluginDescription;
 import au.edu.usq.fascinator.api.PluginException;
 import au.edu.usq.fascinator.api.PluginManager;
@@ -28,32 +38,62 @@ import au.edu.usq.fascinator.api.indexer.Indexer;
 import au.edu.usq.fascinator.api.indexer.IndexerException;
 import au.edu.usq.fascinator.api.indexer.SearchRequest;
 import au.edu.usq.fascinator.common.JsonConfig;
+import au.edu.usq.fascinator.portal.JsonSessionState;
 import au.edu.usq.fascinator.portal.services.IndexerService;
 
 public class IndexerServiceImpl implements IndexerService {
 
     private static final String DEFAULT_INDEXER_TYPE = "solr";
 
+    @Inject
+    private JsonSessionState sessionState;
+
+    private String username;
+
+    /** Messaging services */
+    private MessagingServices messaging;
+
+    /** Logging */
+    private Logger log = LoggerFactory.getLogger(IndexerServiceImpl.class);
+
     private Indexer indexer;
 
-    public IndexerServiceImpl() {
-        try {
-            JsonConfig config = new JsonConfig();
-            indexer = PluginManager.getIndexer(config.get("indexer/type",
-                    DEFAULT_INDEXER_TYPE));
-            indexer.init(JsonConfig.getSystemFile());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    public IndexerServiceImpl(Indexer indexer, ApplicationStateManager asm) {
+
+        if (sessionState == null) {
+            sessionState = asm.get(JsonSessionState.class);
         }
+        username = (String) sessionState.get("username");
+
+        try {
+            messaging = MessagingServices.getInstance();
+        } catch (JMSException jmse) {
+            log.error("Failed to start connection: {}", jmse.getMessage());
+        }
+
+        if (indexer == null) {
+            try {
+                JsonConfig config = new JsonConfig();
+                indexer = PluginManager.getIndexer(config.get("indexer/type",
+                        DEFAULT_INDEXER_TYPE));
+                indexer.init(JsonConfig.getSystemFile());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        this.indexer = indexer;
     }
 
     @Override
     public void index(String oid) throws IndexerException {
+        sentMessage(oid, "modify");
         indexer.index(oid);
     }
 
     @Override
     public void index(String oid, String pid) throws IndexerException {
+        sentMessage(oid, "modify");
         indexer.index(oid, pid);
     }
 
@@ -64,6 +104,7 @@ public class IndexerServiceImpl implements IndexerService {
 
     @Override
     public void annotate(String oid, String pid) throws IndexerException {
+        sentMessage(oid, "modify-anotar");
         indexer.annotate(oid, pid);
     }
 
@@ -75,21 +116,25 @@ public class IndexerServiceImpl implements IndexerService {
 
     @Override
     public void annotateRemove(String oid) throws IndexerException {
+        sentMessage(oid, "delete-anotar");
         indexer.annotateRemove(oid);
     }
 
     @Override
     public void annotateRemove(String oid, String pid) throws IndexerException {
+        sentMessage(oid, "delete-anotar");
         indexer.annotateRemove(oid, pid);
     }
 
     @Override
     public void remove(String oid) throws IndexerException {
+        sentMessage(oid, "delete");
         indexer.remove(oid);
     }
 
     @Override
     public void remove(String oid, String pid) throws IndexerException {
+        sentMessage(oid, "delete");
         indexer.remove(oid, pid);
     }
 
@@ -132,5 +177,15 @@ public class IndexerServiceImpl implements IndexerService {
     @Override
     public void init(String jsonString) throws PluginException {
         indexer.init(jsonString);
+    }
+
+    private void sentMessage(String oid, String eventType) {
+        log.info(" * Sending message: {} with event {}", oid, eventType);
+        Map<String, String> param = new LinkedHashMap<String, String>();
+        param.put("oid", oid);
+        param.put("eventType", eventType);
+        param.put("username", username);
+        param.put("context", indexer.getName());
+        messaging.onEvent(param);
     }
 }
