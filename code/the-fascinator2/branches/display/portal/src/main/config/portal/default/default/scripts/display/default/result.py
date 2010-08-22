f40e42ba097c05bb472252a2f719c0e66ebcad6c
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-08-22 20:28:42 +0000

    refs #1090 created default and image templates for search result object display

---
+++ code/the-fascinator2/branches/display/portal/src/main/config/portal/default/default/scripts/display/default/result.py
@@ -0,0 +1,32 @@
+
+class ObjectResultData:
+    def __init__(self):
+        pass
+    
+    def canManage(self, wfSecurity):
+        user_roles = page.authentication.get_roles_list()
+        for role in user_roles:
+            if role in wfSecurity:
+                return True
+        return False
+    
+    def get(self, name):
+        valueList = metadata.getList(name)
+        if valueList.size() > 0:
+            return valueList.get(0)
+        return ""
+    
+    def getMimeTypeIcon(self, format):
+        # check for specific icon
+        iconPath = "images/icons/mimetype/%s/icon.png" % format
+        resource = Services.getPageService().resourceExists(portalId, iconPath)
+        if resource is not None:
+            return iconPath
+        elif format.find("/") != -1:
+            # check for major type
+            return self.getMimeTypeIcon(format[:format.find("/")])
+        # use default icon
+        return "images/icons/mimetype/icon.png"
+
+if __name__ == "__main__":
+    scriptObject = ObjectResultData()
