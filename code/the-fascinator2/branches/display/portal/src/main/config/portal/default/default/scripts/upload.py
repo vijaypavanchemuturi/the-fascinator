5ffd05b0aa9688313c41840084c360b251d03ecf
pendlebu <pendlebu@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-08-26 02:07:25 +0000

    Refs #1078. Implementing new python caching on workflow/upload screens.

--- code/the-fascinator2/branches/display/portal/src/main/config/portal/default/default/scripts/upload.py
+++ code/the-fascinator2/branches/display/portal/src/main/config/portal/default/default/scripts/upload.py
@@ -1,10 +1,13 @@
-class UploadData:
+class UploadPage:
 
     def __init__(self):
-        self.roles = page.authentication.get_roles_list()
-        self.uploader = toolkit.getFileUploader(self.roles)
+        pass
+
+    def __activate__(self, context):
+        self.vc = context
+
+        self.roles = self.vc["page"].authentication.get_roles_list()
+        self.uploader = self.vc["toolkit"].getFileUploader(self.roles)
 
     def render_upload_form(self):
         return self.uploader.renderForm()
-
-scriptObject = UploadData()
