be4f77b14b53aeb515cb9f7b2838f16ec307764b
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-08-27 02:32:42 +0000

    refs #1063 created empty transformer to perform batch update

---
+++ code/the-fascinator2/branches/octalina/portal/src/main/config/portal/default/default/scripts/batchprocess.py
@@ -0,0 +1,21 @@
+
+class BatchProcess:
+
+    def __init__(self):
+        self.formRenderer = toolkit.getFormRenderer()
+
+    def render_upload_form(self):
+        uploadForm = "<form id='general-form' method='post' action='batchprocess'>\n" \
+                "<fieldset class='login'>\n" \
+                "<legend>Batch Update configuration  File</legend>\n"
+        uploadForm += self.formRenderer.ajaxFluidErrorHolder("upload-file") + "<p>\n"
+        uploadForm += self.formRenderer.renderFormElement("script-file", "text", "Location of the configuration file:") + "</p>\n"
+        
+        uploadForm += self.formRenderer.renderFormElement("upload", "button", "", "Batch Update")
+        uploadForm += self.formRenderer.renderFormElement("cancel", "button", "", "Cancel")
+        uploadForm += self.formRenderer.ajaxProgressLoader("upload-file")
+        uploadForm += "</div></fieldset></form>\n"
+        
+        return uploadForm
+
+scriptObject = BatchProcess()
