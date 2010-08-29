45711363b9423df52322abad07f9077d657beb80
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-08-29 20:51:04 +0000

    refs #1090 #1091 updated metadata for images. updated plain result script as example of how to subclass the default result script.

--- code/the-fascinator2/branches/display/portal/src/main/config/portal/default/default/scripts/display/plain/result.py
+++ code/the-fascinator2/branches/display/portal/src/main/config/portal/default/default/scripts/display/plain/result.py
@@ -1,42 +1,11 @@
+from display.default.result import ResultData as DefaultResultData
+
 from java.io import ByteArrayOutputStream
 from org.apache.commons.io import IOUtils
 
-class ResultData:
-    def __init__(self):
-        pass
-    
+class ResultData(DefaultResultData):
     def __activate__(self, context):
-        self.services = context["Services"]
-        self.page = context["page"]
-        self.metadata = context["metadata"]
-        self.portalId = context["portalId"]
-    
-    def canManage(self):
-        wfSecurity = self.metadata.get("workflow_security")
-        if wfSecurity:
-            user_roles = self.page.authentication.get_roles_list()
-            for role in user_roles:
-                if role in wfSecurity:
-                    return True
-        return False
-
-    def get(self, name):
-        valueList = self.metadata.getList(name)
-        if valueList.size() > 0:
-            return valueList.get(0)
-        return ""
-
-    def getMimeTypeIcon(self, format):
-        # check for specific icon
-        iconPath = "images/icons/mimetype/%s/icon.png" % format
-        resource = self.services.getPageService().resourceExists(self.portalId, iconPath)
-        if resource is not None:
-            return iconPath
-        elif format.find("/") != -1:
-            # check for major type
-            return self.getMimeTypeIcon(format[:format.find("/")])
-        # use default icon
-        return "images/icons/mimetype/icon.png"
+        DefaultResultData.__activate__(self, context)
 
     def getSourceSample(self, id, limit):
         # Get source payload
