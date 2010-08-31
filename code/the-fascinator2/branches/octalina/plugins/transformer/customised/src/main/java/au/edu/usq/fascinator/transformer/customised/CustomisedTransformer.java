1a21e07089a5c510e6d538c33c9b129d24d46b25
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-08-31 03:34:08 +0000

    removing target directory

--- code/the-fascinator2/branches/octalina/plugins/transformer/customised/src/main/java/au/edu/usq/fascinator/transformer/customised/CustomisedTransformer.java
+++ code/the-fascinator2/branches/octalina/plugins/transformer/customised/src/main/java/au/edu/usq/fascinator/transformer/customised/CustomisedTransformer.java
@@ -28,10 +28,13 @@ import org.slf4j.LoggerFactory;
 
 import au.edu.usq.fascinator.api.PluginDescription;
 import au.edu.usq.fascinator.api.PluginException;
+import au.edu.usq.fascinator.api.indexer.IndexerException;
 import au.edu.usq.fascinator.api.storage.DigitalObject;
 import au.edu.usq.fascinator.api.transformer.Transformer;
 import au.edu.usq.fascinator.api.transformer.TransformerException;
+import au.edu.usq.fascinator.common.JsonConfig;
 import au.edu.usq.fascinator.common.JsonConfigHelper;
+import au.edu.usq.fascinator.common.PythonUtils;
 
 /**
  * Provides methods for batch processing of digital objects
@@ -45,6 +48,10 @@ public class CustomisedTransformer implements Transformer {
 
     /** Json config file **/
     private JsonConfigHelper config;
+
+    private JsonConfig jsonConfig;
+
+    private PythonUtils pyUtils;
 
     /**
      * Extractor Constructor
@@ -62,6 +69,7 @@ public class CustomisedTransformer implements Transformer {
     public void init(String jsonString) throws PluginException {
         try {
             config = new JsonConfigHelper(jsonString);
+            jsonConfig = new JsonConfig(jsonString);
             reset();
         } catch (IOException e) {
             throw new PluginException(e);
@@ -78,6 +86,7 @@ public class CustomisedTransformer implements Transformer {
     public void init(File jsonFile) throws PluginException {
         try {
             config = new JsonConfigHelper(jsonFile);
+            jsonConfig = new JsonConfig(jsonFile);
             reset();
         } catch (IOException e) {
             throw new PluginException(e);
@@ -89,6 +98,11 @@ public class CustomisedTransformer implements Transformer {
      */
     private void reset() throws TransformerException {
         log.info("--Initializing Extractor plugin--");
+        try {
+            pyUtils = new PythonUtils(jsonConfig);
+        } catch (PluginException e) {
+            log.error("Fail to initialise pyUtils");
+        }
     }
 
     /**
@@ -147,30 +161,52 @@ public class CustomisedTransformer implements Transformer {
             String scriptFile = json.get("transformer/customised/script",
                     props.getProperty("customisedScript"));
             if (scriptFile != null) {
+                File script = new File(scriptFile);
+                if (!script.exists()) {
+                    log.error("Script file not found: '{}'", scriptFile);
+                    restoreAllProperty(props);
+                    return in;
+                }
+
                 log.info("Running script: '{}'", scriptFile);
                 // Run the script
                 PythonInterpreter python = new PythonInterpreter();
                 python.set("object", in);
                 python.set("config", json);
+                python.set("pyUtils", pyUtils);
                 python.execfile(scriptFile);
                 python.cleanup();
                 // Modify the property
                 props.setProperty("modified", "true");
             } else {
-                log.info("");
+                log.info("Script file not found");
             }
+            restoreAllProperty(props);
 
-            restoreProperty(props, "indexOnHarvest");
-            restoreProperty(props, "harvestQueue");
-            restoreProperty(props, "renderQueue");
-            restoreProperty(props, "customisedScript");
         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
         return in;
     }
 
+    /**
+     * Restore all properties
+     * 
+     * @param props properties of an object
+     */
+    private void restoreAllProperty(Properties props) {
+        restoreProperty(props, "indexOnHarvest");
+        restoreProperty(props, "harvestQueue");
+        restoreProperty(props, "renderQueue");
+        restoreProperty(props, "customisedScript");
+    }
+
+    /**
+     * Restoring individual property and removing the unnecessary property
+     * 
+     * @param props properties of an object
+     * @param key property to be restored
+     */
     private void restoreProperty(Properties props, String key) {
         String copyKey = "copyOf_" + key;
         if (props.containsKey(copyKey)) {
