c67f9595c952001393dd7aeadcd6660f16701b0c
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-09-08 01:30:09 +0000

    completing export to fedora draft script refs #1077

--- code/the-fascinator2/contrib/fascinator/plugins/harvester/fedora2/trunk/src/main/resources/batch-process/export/exportFedora2.py
+++ code/the-fascinator2/contrib/fascinator/plugins/harvester/fedora2/trunk/src/main/resources/batch-process/export/exportFedora2.py
@@ -1,91 +1,108 @@
+"""*** NOTE: this is a draft version of export to fedora script. 
+You might need to modify the script to fit your needs.
+
+Based on payloadToExport values, this script will create the datastream 
+if not exist in fedora or replace the existing datastream if it's found.
+
+Based on payloadToDelete values, this script will also remove the payload 
+from fascinator and datastream from fedora.
+"""
+
 from fedora.client import FedoraClient
 from org.apache.commons.io import IOUtils
 from au.edu.usq.fascinator.harvester.fedora.restclient import FedoraRestClient
-from java.io import File
+from java.io import File, FileOutputStream
+from java.lang import String
+import time
 
-print "** Export to Fedora2 (draft)"
+def checkIfDsExist(apim, fedoraId, value, dsState):
+    dataStreams = apim.getDatastreams(fedoraId, value, dsState)
+    found = False 
+    foundDs = None
+    for ds in dataStreams:
+        if ds.getID()==value:
+            found = True
+            foundDs = ds
+            break 
+    return found, foundDs
 
-client = FedoraClient("http://localhost:8001/fedora/", "fedoraAdmin", "fedoraAdmin")
+print "** Export to Fedora2 **"
+
+client = FedoraClient("http://localhost:8001/fedora/", "fedoraAdmin", "fedoraAdmin") 
 apim = client.getAPIM()
-restClient = FedoraRestClient("http://localhost:8001")
+restClient = FedoraRestClient("http://localhost:8001/fedora")
 
 #Delete the payload that is supposed to be deleted
 props = object.getMetadata()
 e = props.propertyNames()
+fedoraId = props.getProperty("fedoraPid")
+fedoraId = "uon:20"
 while (e.hasMoreElements()):
     key = str(e.nextElement())
     value = props.getProperty(key)
     if key.startswith("payloadToDelete."):
         #Notify fedora to delete this datastream
-        print " ** Deleting datastream: ", value
-        apim.purgeDatastream(object.getId(), value, None, None, "Deleting from fascinator", False)
+        print " * Deleting datastream: '%s'" % value
+        #check if datastream exist
+        found, foundDs = checkIfDsExist(apim, fedoraId, value, dsState)
+        if found:
+            apim.purgeDatastream(fedoraId, foundDs.getID(), None, None, "Deleting datastream %s" % foundDs.getID(), False)
+            print " * Deleted '%s'" % value 
+        else:
+            print " * '%s' datastream not exist in '%s' object" % (value, fedoraId) 
         
         #Notify fascinator to delete the payload
         try:
-            pass
-            #object.removePayload(props.getProperty(key))
+            object.removePayload(props.getProperty(key))
         except:
-            print "fail to delete payload"
+            print "Fail to delete payload from fascinator: '%s'" % value
         props.remove(key)
     if key.startswith("payloadToExport."):
         #Notify fedora to add this datastream
-        print " ** Deleting datastream: ", value
-        
+        print " ** Exporting modified datastream: ", value
         payload = object.getPayload(value)
         #Check if object exist
-        objectResult = restClient.findObjects(object.getId(), 1)
-        print "result is empty: ", objectResult.getObjectFields().isEmpty()
+        objectResult = restClient.findObjects(fedoraId, 1)
         if not objectResult.getObjectFields().isEmpty():
             try:
                 mimeType = payload.getContentType()
                 formatURI = None
                 dsLocation = None
-                controlGroup = "M"  # get original instead of setting
-                dsState = "A"  # get original instead of setting
+                dsState = "A"
                 #Check if datastream exist
-                datastream = apim.getDatastream(object.getId(), value, None)
-                if datastream:
-                    byteArrayOfDSContent=IOUtils.toByteArray(payload.open())
-                    apim.modifyDatastreamByValue(object.getId(), value, None, value, mimeType, \
-                                                 formatURI, byteArrayOfDSContent, None, None, \
-                                                 "Updating datastream from Fascinator", False)
-                else:
-                    f = File.createTempFile("fedora-", ".tmp")
-                    fOut = FileOutputStream()
+                print "Getting datastream for: ", value
+                found, foundDs = checkIfDsExist(apim, fedoraId, value, dsState)
+                if found:
+                    print " * Modifying: '%s'" % value
+                    f = File.createTempFile("fedora-", ".xml")
+                    fOut = FileOutputStream(f)
                     IOUtils.copy(payload.open(), fOut)
                     payload.close()
                     fOut.close()
                     dsLocation = client.uploadFile(f)
-                    #add new datastream
-                    apim.addDatastream(object.getId(), value, None, mimeType, True, mimeType, \
-                                       formatURI, dsLocation, controlGroup, dsState, None, None, \
-                                       "Creating new datastream")
-            except:
+                    dsId = apim.modifyDatastreamByReference(fedoraId, foundDs.getID(), foundDs.getAltIDs(), foundDs.getLabel(), \
+                                                     mimeType, foundDs.getFormatURI(), dsLocation, foundDs.getChecksumType(), foundDs.getChecksum(), \
+                                                     "Updating datastream from Fascinator", True)
+                    print " * Modified: '%s'" % dsId
+                else:
+                    print " * Adding: '%s'" % value
+                    f = File.createTempFile("fedora-", ".xml")
+                    fOut = FileOutputStream(f)
+                    IOUtils.copy(payload.open(), fOut)
+                    payload.close()
+                    fOut.close()
+                    dsLocation = client.uploadFile(f)
+                    controlGroup = "M"  
+                    dsId = apim.addDatastream(fedoraId, value, None, value, True, mimeType, formatURI, dsLocation, controlGroup, dsState, None, None, "Creating new datastream")
+                    print " * Added datastream: '%s'" % dsId
+                    
+                #set the value of last exported
+                props.setProperty("lastExported", time.strftime("%Y-%m-%dT%H:%M:%SZ"))
+            except Exception, e:
                 #Remote exception
-                pass
+                print "Error: ", str(e)
         else:
-            formatURI = None
-            dsLocation = None
-            controlGroup = "M"  # get original instead of setting
-            dsState = "A"  # get original instead of setting
-            # Create the object and the datastream
-            xmlContent = IOUtils.toByteArray(client.getClass().getResourceAsStream("foxml11_template.xml"))
-            apim.ingest(xmlContent, "info:fedora/fedora-system:FOXML-1.1", "Creating new object")
-            #adding each datastream
-            for payload in object.getPayloadList():
-                mimeType = payload.getContentType()
-                f = File.createTempFile("fedora-", ".tmp")
-                fOut = FileOutputStream()
-                IOUtils.copy(payload.open(), fOut)
-                payload.close()
-                fOut.close()
-                dsLocation = client.uploadFile(f)
-                apim.addDatastream(object.getId(), value, None, mimeType, True, mimeType, \
-                                       formatURI, dsLocation, controlGroup, dsState, None, None, \
-                                       "Creating new dataastream")
-            
-            apim.ingest()
-        
+            print "Object id: '%s' is not exist in Fedora" % fedoraPid
         props.remove(key)
 
 #reset modified property after export
