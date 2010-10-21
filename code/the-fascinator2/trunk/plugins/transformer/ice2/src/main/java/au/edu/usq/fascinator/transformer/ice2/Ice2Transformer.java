fe8ec4983fe7271e4d88f1097316238ef3a11a9e
pendlebu <pendlebu@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-10-21 00:46:33 +0000

    Updated ICE transformer to validate objects re: two thumbnails/previews.

--- code/the-fascinator2/trunk/plugins/transformer/ice2/src/main/java/au/edu/usq/fascinator/transformer/ice2/Ice2Transformer.java
+++ code/the-fascinator2/trunk/plugins/transformer/ice2/src/main/java/au/edu/usq/fascinator/transformer/ice2/Ice2Transformer.java
@@ -1,5 +1,20 @@
 package au.edu.usq.fascinator.transformer.ice2;
 
+import au.edu.usq.fascinator.api.PluginDescription;
+import au.edu.usq.fascinator.api.PluginException;
+import au.edu.usq.fascinator.api.storage.DigitalObject;
+import au.edu.usq.fascinator.api.storage.Payload;
+import au.edu.usq.fascinator.api.storage.PayloadType;
+import au.edu.usq.fascinator.api.storage.StorageException;
+import au.edu.usq.fascinator.api.transformer.Transformer;
+import au.edu.usq.fascinator.api.transformer.TransformerException;
+import au.edu.usq.fascinator.common.BasicHttpClient;
+import au.edu.usq.fascinator.common.JsonConfig;
+import au.edu.usq.fascinator.common.JsonConfigHelper;
+import au.edu.usq.fascinator.common.MimeTypeUtil;
+import au.edu.usq.fascinator.common.sax.SafeSAXReader;
+import au.edu.usq.fascinator.common.storage.StorageUtils;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,11 +22,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -31,21 +46,6 @@ import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import au.edu.usq.fascinator.api.PluginDescription;
-import au.edu.usq.fascinator.api.PluginException;
-import au.edu.usq.fascinator.api.storage.DigitalObject;
-import au.edu.usq.fascinator.api.storage.Payload;
-import au.edu.usq.fascinator.api.storage.PayloadType;
-import au.edu.usq.fascinator.api.storage.StorageException;
-import au.edu.usq.fascinator.api.transformer.Transformer;
-import au.edu.usq.fascinator.api.transformer.TransformerException;
-import au.edu.usq.fascinator.common.BasicHttpClient;
-import au.edu.usq.fascinator.common.JsonConfig;
-import au.edu.usq.fascinator.common.JsonConfigHelper;
-import au.edu.usq.fascinator.common.MimeTypeUtil;
-import au.edu.usq.fascinator.common.sax.SafeSAXReader;
-import au.edu.usq.fascinator.common.storage.StorageUtils;
 
 /**
  * Transformer Class will send a file to ice-service to get the renditions of
@@ -113,6 +113,12 @@ public class Ice2Transformer implements Transformer {
 
     /** Exclude these file from renditions */
     private List<String> excludeList;
+
+    /** A list of thumbnails in the object */
+    private List<String> thumbnails;
+
+    /** A list of previews in the object */
+    private List<String> previews;
 
     /**
      * ICE transformer constructor
@@ -184,6 +190,10 @@ public class Ice2Transformer implements Transformer {
 
         // Clear the old SAX reader
         reader = new SafeSAXReader();
+
+        // Remove the last object
+        thumbnails = null;
+        previews = null;
     }
 
     /**
@@ -226,13 +236,12 @@ public class Ice2Transformer implements Transformer {
         } catch (IOException ex) {
             log.error("Error writing temp file : ", ex);
             return object;
-            // throw new TransformerException(ex);
         } catch (StorageException ex) {
             log.error("Error accessing storage data : ", ex);
             return object;
-            // throw new TransformerException(ex);
         }
 
+        // Render the file if supported
         if (file.exists() && !excludeList.contains(ext.toLowerCase())) {
             try {
                 if (isSupported(file)) {
@@ -241,22 +250,17 @@ public class Ice2Transformer implements Transformer {
                     object = createIcePayload(object, outputFile);
                     outputFile.delete();
                 }
-            } catch (TransformerException te) {
+            } catch (Exception e) {
                 log.debug("Adding error payload to {}", object.getId());
                 try {
-                    object = createErrorPayload(object, fileName, te
-                            .getMessage());
-                } catch (StorageException e) {
-                    e.printStackTrace();
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
+                    object = createErrorPayload(object, fileName, e);
+                } catch (Exception e1) {
+                    log.error("Error creating error payload", e1);
                 }
-            } catch (StorageException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
+
+        // Cleanup an finish
         try {
             object.close();
         } catch (StorageException ex) {
@@ -279,9 +283,13 @@ public class Ice2Transformer implements Transformer {
      * @throws UnsupportedEncodingException
      */
     public DigitalObject createErrorPayload(DigitalObject object, String file,
-            String message) throws StorageException,
+            Exception ex) throws StorageException,
             UnsupportedEncodingException {
         String name = file + "_ice_error.htm";
+        String message = ex.getMessage();
+        if (message == null) {
+            message = ex.toString();
+        }
         Payload errorPayload = StorageUtils.createOrUpdatePayload(object, name,
                 new ByteArrayInputStream(message.getBytes("UTF-8")));
         errorPayload.setType(PayloadType.Error);
@@ -300,8 +308,7 @@ public class Ice2Transformer implements Transformer {
      * @throws IOException
      */
     public DigitalObject createIcePayload(DigitalObject object, File file)
-            throws StorageException, IOException {
-        String previewPayloadId = "";
+            throws StorageException, IOException, Exception {
         if (ZIP_MIME_TYPE.equals(MimeTypeUtil.getMimeType(file))) {
             ZipFile zipFile = new ZipFile(file);
             Enumeration<? extends ZipEntry> entries = zipFile.entries();
@@ -331,7 +338,7 @@ public class Ice2Transformer implements Transformer {
                             // Prep our inputstream again
                             in = new ByteArrayInputStream(out.toByteArray());
                         } catch (DocumentException ex) {
-                            createErrorPayload(object, name, ex.getMessage());
+                            createErrorPayload(object, name, ex);
                             continue;
                         } catch (Exception ex) {
                             log.error("Error : ", ex);
@@ -339,20 +346,24 @@ public class Ice2Transformer implements Transformer {
                         }
                     }
 
+                    // Determing the payload type to use in storage
+                    PayloadType pt = null;
+                    try {
+                        pt = assignType(object, name, mimeType);
+                    } catch (TransformerException ex) {
+                        throw new Exception(
+                                "Error examining object to assign type: ", ex);
+                    }
+                    if (pt == null) {
+                        // We're done, this file is not being stored
+                        return object;
+                    }
+
                     Payload icePayload = StorageUtils.createOrUpdatePayload(
                             object, name, in);
                     icePayload.setLabel(name);
                     icePayload.setContentType(mimeType);
-                    if (mimeType.equals(HTML_MIME_TYPE)
-                            || (mimeType.contains(IMG_MIME_TYPE) && name
-                                    .contains("_preview"))) {
-                        previewPayloadId = icePayload.getId();
-                        // icePayload.setType(PayloadType.Preview);
-                    }
-                    if (mimeType.contains(IMG_MIME_TYPE)
-                            && name.contains("_thumbnail.jpg")) {
-                        icePayload.setType(PayloadType.Thumbnail);
-                    }
+                    icePayload.setType(pt);
                     icePayload.close();
                 }
             }
@@ -360,59 +371,174 @@ public class Ice2Transformer implements Transformer {
         } else {
             String name = file.getName();
             String mimeType = MimeTypeUtil.getMimeType(name);
-            // log.debug("Name : '" + name + "', MimeType : '" + mimeType +
-            // "'");
+            // Determing the payload type to use in storage
+            PayloadType pt = null;
+            try {
+                pt = assignType(object, name, mimeType);
+            } catch (TransformerException ex) {
+                throw new Exception(
+                        "Error examining object to assign type: ", ex);
+            }
+            if (pt == null) {
+                // We're done, this file is not being stored
+                return object;
+            }
+
             Payload icePayload = StorageUtils.createOrUpdatePayload(object,
                     name, new FileInputStream(file));
             icePayload.setLabel(name);
             icePayload.setContentType(mimeType);
-            if (mimeType.equals(HTML_MIME_TYPE)
-                    || (mimeType.contains(IMG_MIME_TYPE) && name
-                            .contains("_preview"))) {
-                previewPayloadId = icePayload.getId();
-                // icePayload.setType(PayloadType.Preview);
-            }
-            if (mimeType.contains(IMG_MIME_TYPE)
-                    && name.contains("_thumbnail.jpg")) {
-                icePayload.setType(PayloadType.Thumbnail);
-            }
+            icePayload.setType(pt);
             icePayload.close();
         }
 
-        // Set preview Payload type to the specified payload
-        // Check if preview Payload is exist
-        Boolean hasPreview = false;
-        String existingPreviewPayloadId = "";
-        Set<String> payloadIds = object.getPayloadIdList();
-        for (String payloadId : payloadIds) {
-            Payload payload = object.getPayload(payloadId);
-            if (payload.getType() == PayloadType.Preview) {
-                hasPreview = true;
-                existingPreviewPayloadId = payloadId;
-            }
-        }
-
-        // If has preview payloads and payloads in ice transformer is
-        // the priority, then set the existing payload to AltPreview
-        Payload previewPayload = object.getPayload(previewPayloadId);
-        if (hasPreview) {
-            if (priority) {
-                Payload existingPreviewPayload = object
-                        .getPayload(existingPreviewPayloadId);
-                existingPreviewPayload.setType(PayloadType.AltPreview);
-                existingPreviewPayload.close();
-
-                previewPayload.setType(PayloadType.Preview);
-            } else {
-                previewPayload.setType(PayloadType.AltPreview);
-            }
-        } else {
-            previewPayload.setType(PayloadType.Preview);
-
-        }
-        previewPayload.close();
-
         return object;
+    }
+
+    /**
+     * After assessing the existing object and what needs to be added, return a
+     * PayloadType to use for new payloads.
+     *
+     * @param object: The object to add a payload to
+     * @param pid: The new payload ID that will be used
+     * @param mimeType: The MIME type of the content being added
+     * @return PayloadType: The type to allocate to the new payload
+     */
+    private PayloadType assignType(DigitalObject object, String pid,
+            String mimeType) throws TransformerException {
+        // First run through for the object
+        if (thumbnails == null) {
+            getThumbAndPreviews(object);
+            cleanObject(object);
+        }
+
+        // Have we seen it before?
+        if (thumbnails.contains(pid)) {
+            return PayloadType.Thumbnail;
+        }
+        if (previews.contains(pid)) {
+            return PayloadType.Preview;
+        }
+
+        // Previews
+        if (mimeType.equals(HTML_MIME_TYPE) ||
+           ((mimeType.contains(IMG_MIME_TYPE) && pid.contains("_preview")))) {
+            // Existing previews?
+            if (!previews.isEmpty()) {
+                // Do we have priority?
+                if (priority) {
+                    // Yep, bump the old payload
+                    String oldPid = previews.get(0);
+                    changeType(object, oldPid, PayloadType.AltPreview);
+                    previews.remove(oldPid);
+                    // And add the new
+                    previews.add(pid);
+                    return PayloadType.Preview;
+                } else {
+                    // No, just and a new alt
+                    return PayloadType.AltPreview;
+                }
+            } else {
+                // Simple, the first preview
+                previews.add(pid);
+                return PayloadType.Preview;
+            }
+        }
+
+        // Thumbnails
+        if ((mimeType.contains(IMG_MIME_TYPE) &&
+                pid.contains("_thumbnail.jpg"))) {
+            // Existing previews?
+            if (!thumbnails.isEmpty()) {
+                // Do we have priority?
+                if (priority) {
+                    // Yep, bump the old payload
+                    String oldPid = thumbnails.get(0);
+                    changeType(object, oldPid, PayloadType.Enrichment);
+                    thumbnails.remove(oldPid);
+                    // And add the new
+                    thumbnails.add(pid);
+                    return PayloadType.Thumbnail;
+                } else {
+                    // No, we are going to ignore this one then
+                    return null;
+                }
+            } else {
+                // Simple, the first thumbnail
+                thumbnails.add(pid);
+                return PayloadType.Thumbnail;
+            }
+        }
+
+        // Not sure what it is, so just use Enrichment
+        return PayloadType.Enrichment;
+    }
+
+    /**
+     * Remove extraneous thumbnails and previews from the object if found
+     *
+     * @param object: The object to clean
+     */
+    private void cleanObject(DigitalObject object) throws TransformerException {
+        boolean success;
+
+        // Validate thumbnails
+        if (thumbnails.size() > 1) {
+            // TODO: We could could some complicated logic here guessing where
+            //  things came from... or we could just keep the first one.
+            String keeper = thumbnails.get(0);
+            // Avoid concurrent modification
+            String[] loop = thumbnails.toArray(new String[0]);
+            for (String pid : loop) {
+                if (!pid.equals(keeper)) {
+                    success = changeType(object, pid, PayloadType.Enrichment);
+                    if (!success) {
+                        throw new TransformerException("Object has multiple " +
+                             "thumbnails, error accessing payloads to correct");
+                    }
+                    thumbnails.remove(pid);
+                }
+            }
+        }
+
+        // Validate previews
+        if (previews.size() > 1) {
+            // TODO: We could could some complicated logic here guessing where
+            //  things came from... or we could just keep the first one.
+            String keeper = previews.get(0);
+            String[] loop = previews.toArray(new String[0]);
+            for (String pid : loop) {
+                if (!pid.equals(keeper)) {
+                    success = changeType(object, pid, PayloadType.AltPreview);
+                    if (!success) {
+                        throw new TransformerException("Object has multiple " +
+                               "previews, error accessing payloads to correct");
+                    }
+                    previews.remove(pid);
+                }
+            }
+        }
+    }
+
+    /**
+     * Change the type of an existing payload in the object.
+     *
+     * @param object: The object containing the payload
+     * @param pid: The payload ID of the payload to change
+     * @param newType: The new type to allocate
+     * @return boolean: True if the change was successful, False if not
+     */
+    private boolean changeType(DigitalObject object, String pid,
+            PayloadType newType) {
+        try {
+            Payload payload = object.getPayload(pid);
+            payload.setType(newType);
+            payload.close();
+            return true;
+        } catch (StorageException ex) {
+            log.error("Error accessing payload: '{}'", pid, ex);
+            return false;
+        }
     }
 
     /**
@@ -432,10 +558,10 @@ public class Ice2Transformer implements Transformer {
         // Grab our config
         Map<String, JsonConfigHelper> resizeConfig = itemConfig
                 .getJsonMap("resize");
-        if (resizeConfig == null || resizeConfig.size() == 0) {
+        if (resizeConfig == null || resizeConfig.isEmpty()) {
             // Try system config instead
             resizeConfig = config.getJsonMap("resize");
-            if (resizeConfig == null || resizeConfig.size() == 0) {
+            if (resizeConfig == null || resizeConfig.isEmpty()) {
                 throw new TransformerException(
                         "No resizing configuration found.");
             }
@@ -603,5 +729,33 @@ public class Ice2Transformer implements Transformer {
             configEntry = config.get(key, value);
         }
         return configEntry;
+    }
+
+    /**
+     * Retrieve a list of payloads that have the type 'Thumbnail' or 'Preview'.
+     * In theory this should only ever be zero or one of each, but we are going
+     * to also validate 'broken' objects.
+     *
+     * @param object: The object to retrieve thumbnails for
+     */
+    private void getThumbAndPreviews(DigitalObject object) {
+        thumbnails = new ArrayList();
+        previews = new ArrayList();
+        // Loop through all payloads
+        for (String pid : object.getPayloadIdList()) {
+            try {
+                Payload p = object.getPayload(pid);
+                // Compare their type
+                if (p.getType().compareTo(PayloadType.Thumbnail) == 0) {
+                    thumbnails.add(pid);
+                }
+                // Compare their type
+                if (p.getType().compareTo(PayloadType.Preview) == 0) {
+                    previews.add(pid);
+                }
+            } catch (StorageException ex) {
+                log.error("Error looking at payload: '{}'", pid);
+            }
+        }
     }
 }
