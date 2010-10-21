02e733567d568b23854b4faefb0528995be2a816
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-10-21 06:03:45 +0000

    refs #1158 added queue consumer to send emails for notification. also added message queue testing to queues admin page.

--- code/the-fascinator2/trunk/portal/src/main/config/portal/default/default/scripts/queues.py
+++ code/the-fascinator2/trunk/portal/src/main/config/portal/default/default/scripts/queues.py
@@ -1,12 +1,23 @@
-from au.edu.usq.fascinator.common import JsonConfig
-from au.edu.usq.fascinator.common import JsonConfigHelper
+from au.edu.usq.fascinator.common import JsonConfig, JsonConfigHelper
+from au.edu.usq.fascinator.common import MessagingServices
 
 class QueuesData:
     def __init__(self):
         pass
 
     def __activate__(self, context):
-        self.velocityContext = context
+        self.request = context["request"]
+        self.response = context["response"]
+        self.formData = context["formData"]
+
+        if self.request.isXHR():
+            print " **** formData: %s" % self.formData
+            queue = self.formData.get("queueName")
+            msg = self.formData.get("queueMessage")
+            self.queueMessage(queue, msg);
+            out = self.response.getPrintWriter("text/plain")
+            out.println(self.formData)
+            out.close()
 
         self.config = JsonConfigHelper(JsonConfig.getSystemFile())
         self.threads = self.config.getJsonList("messaging/threads")
@@ -16,3 +27,9 @@ class QueuesData:
             name = thread.get("config/name")
             if name == queue:
                 return thread.get("description")
+
+    def queueMessage(self, queue, msg):
+        ms = MessagingServices.getInstance()
+        ms.queueMessage(queue, msg);
+        ms.release()
+
