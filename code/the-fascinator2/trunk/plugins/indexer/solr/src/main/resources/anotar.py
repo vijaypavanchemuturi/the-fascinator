486979e051be6ddcde707e84189bb4128774f897
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-10-29 06:46:13 +0000

    refs #1207 video/audio annotaion using jwplayer

--- code/the-fascinator2/trunk/plugins/indexer/solr/src/main/resources/anotar.py
+++ code/the-fascinator2/trunk/plugins/indexer/solr/src/main/resources/anotar.py
@@ -1,59 +1,81 @@
-from au.edu.usq.fascinator.indexer.rules import AddField, New
+from au.edu.usq.fascinator.common import JsonConfigHelper
 
-#
-# Available objects:
-#    indexer    : Indexer instance
-#    jsonConfig : JsonConfigHelper of our harvest config file
-#    rules      : RuleManager instance
-#    object     : DigitalObject to index
-#    payload    : Payload to index
-#    params     : Metadata Properties object
-#    pyUtils    : Utility object for accessing app logic
-#
+class IndexData:
+    def __init__(self):
+        pass
 
-# Read the payload json string into
-# a JsonConfigHelper object.
-json = pyUtils.getJsonObject(payload.open())
-payload.close()
+    def __activate__(self, context):
+        # Prepare variables
+        self.index = context["fields"]
+        self.object = context["object"]
+        self.payload = context["payload"]
+        self.params = context["params"]
+        self.utils = context["pyUtils"]
+        self.indexer = context["indexer"]
 
-#start with blank solr document
-rules.add(New())
+        # Common data
+        self.__newDoc()
+        
+        ##self.__security()
 
-rules.add(AddField("schemaVersion", json.get("schemaVersionUri")))
-rules.add(AddField("clientVersion", json.get("clientVersionUri")))
-rules.add(AddField("id", json.get("id")))
-rules.add(AddField("uri", json.get("uri")))
-rules.add(AddField("type", json.get("type")))
-rules.add(AddField("titleLiteral", json.get("title/literal")))
-rules.add(AddField("titleUri", json.get("title/uri")))
-rules.add(AddField("annotatesLiteral", json.get("annotates/literal")))
-rules.add(AddField("annotatesUri", json.get("annotates/uri")))
-rules.add(AddField("rootUri", json.get("annotates/rootUri")))
-rules.add(AddField("creatorLiteral", json.get("creator/literal")))
-rules.add(AddField("creatorUri", json.get("creator/uri")))
-rules.add(AddField("creatorEmail", json.get("creator/email/literal")))
-rules.add(AddField("creatorEmailMd5", json.get("creator/email/md5hash")))
-rules.add(AddField("contentType", json.get("content/mimeType")))
-rules.add(AddField("contentLiteral", json.get("content/literal")))
-rules.add(AddField("isPrivate", json.get("isPrivate")))
-rules.add(AddField("lang", json.get("lang")))
+    def __newDoc(self):
+        # Read the payload json string into
+        # a JsonConfigHelper object.
+        self.oid = self.object.getId()
+        self.pid = self.payload.getId()
+        
+        json = self.utils.getJsonObject(self.payload.open())
+        self.payload.close()
 
-# Date handling, Solr only accepts UTC
-#http://lucene.apache.org/solr/api/org/apache/solr/schema/DateField.html
-dateCreated = json.get("dateCreated/literal")
-if dateCreated is not None:
-    rules.add(AddField("dateCreated", dateCreated[:19] + "Z"))
-    rules.add(AddField("tzCreated", dateCreated[19:]))
-dateModified = json.get("dateModified/literal")
-if dateModified is not None:
-    rules.add(AddField("dateModified", dateModified[:19] + "Z"))
-    rules.add(AddField("tzModified", dateModified[19:]))
+        self.utils.add(self.index, "schemaVersion", json.get("schemaVersionUri"))
+        self.utils.add(self.index, "clientVersion", json.get("clientVersionUri"))
+        self.utils.add(self.index, "id", json.get("id"))
+        self.utils.add(self.index, "uri", json.get("uri"))
+        self.utils.add(self.index, "type", json.get("type"))
+        self.utils.add(self.index, "titleLiteral", json.get("title/literal"))
+        self.utils.add(self.index, "titleUri", json.get("title/uri"))
+        self.utils.add(self.index, "annotatesLiteral", json.get("annotates/literal")) ##
+        self.utils.add(self.index, "annotatesUri", json.get("annotates/uri"))
+        self.utils.add(self.index, "rootUri", json.get("annotates/rootUri"))
+        self.utils.add(self.index, "creatorLiteral", json.get("creator/literal")) ##
+        self.utils.add(self.index, "creatorUri", json.get("creator/uri"))
+        self.utils.add(self.index, "creatorEmail", json.get("creator/email/literal"))
+        self.utils.add(self.index, "creatorEmailMd5", json.get("creator/email/md5hash"))
+        self.utils.add(self.index, "contentType", json.get("content/mimeType"))
+        self.utils.add(self.index, "contentLiteral", json.get("content/literal"))
+        self.utils.add(self.index, "isPrivate", json.get("isPrivate"))
+        self.utils.add(self.index, "lang", json.get("lang"))
 
-# Arrays
-for locator in json.getJsonList("annotates/locators"):
-    rules.add(AddField("locators", locator.toString()))
-    rules.add(AddField("locatorValue", locator.get("value")))
-    rules.add(AddField("locatorContent", locator.get("originalContent")))
+        # Date handling, Solr only accepts UTC
+        #http://lucene.apache.org/solr/api/org/apache/solr/schema/DateField.html
+        dateCreated = json.get("dateCreated/literal")
+        if dateCreated is not None:
+            self.utils.add(self.index, "dateCreated", dateCreated[:19] + "Z")
+            self.utils.add(self.index, "tzCreated", dateCreated[19:])
+        dateModified = json.get("dateModified/literal")
+        if dateModified is not None:
+            self.utils.add(self.index, "dateModified", dateModified[19:] + "Z")
+            self.utils.add(self.index, "tzModified", dateModified[19:])
+            
+        # Arrays
+        for locator in json.getJsonList("annotates/locators"):
+            self.utils.add(self.index, "locators", locator.toString())
+            self.utils.add(self.index, "locatorValue", locator.get("value"))
+            self.utils.add(self.index, "locatorContent", locator.get("originalContent"))
 
-# Our full string
-rules.add(AddField("jsonString", json.toString(False)))
+        # Our full string
+        self.utils.add(self.index, "jsonString", json.toString(False))
+
+
+    def __security(self):
+        roles = self.utils.getRolesWithAccess(self.oid)
+        if roles is not None:
+            for role in roles:
+                self.utils.add(self.index, "security_filter", role)
+        else:
+            # Default to guest access if Null object returned
+            schema = self.utils.getAccessSchema("derby");
+            schema.setRecordId(self.oid)
+            schema.set("role", "guest")
+            self.utils.setAccessSchema(schema, "derby")
+            self.utils.add(self.index, "security_filter", "guest")
