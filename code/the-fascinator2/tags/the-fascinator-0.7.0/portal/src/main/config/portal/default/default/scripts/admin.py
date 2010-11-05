f053ca845aa554faca7d95c11d4c6263fa127587
hudson <hudson@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-11-05 06:09:06 +0000

    [maven-release-plugin]  copy for tag the-fascinator-0.7.0

---
+++ code/the-fascinator2/tags/the-fascinator-0.7.0/portal/src/main/config/portal/default/default/scripts/admin.py
@@ -0,0 +1,13 @@
+from au.edu.usq.fascinator.common import JsonConfigHelper
+
+class AdminData:
+
+    def __init__(self):
+        pass
+
+    def __activate__(self, context):
+        self.velocityContext = context
+        self.json = JsonConfigHelper()
+
+    def parse_json(self, json_string):
+        self.json = JsonConfigHelper(json_string)
