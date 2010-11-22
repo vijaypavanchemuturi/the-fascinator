b6454228c5a59665c3241249e193a43d61fcfa3a
octalina <octalina@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-11-22 06:27:44 +0000

    wrong release version

--- code/the-fascinator2/tags/the-fascinator-1.7.2/portal/src/main/config/portal/default/default/scripts/display/word-processing/result.py
+++
@@ -1,5 +0,0 @@
-from display.default.result import ResultData as DefaultResultData
-
-class ResultData(DefaultResultData):
-    def __activate__(self, context):
-        DefaultResultData.__activate__(self, context)
