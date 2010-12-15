d5561b387450a74924020555a5b389a504bf9648
pendlebu <pendlebu@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2010-12-15 04:15:05 +0000

    Refs #1338. authtest interface changed use JSONP.

--- code/the-fascinator2/trunk/portal/src/main/config/portal/default/default/scripts/actions/authtest.py
+++ code/the-fascinator2/trunk/portal/src/main/config/portal/default/default/scripts/actions/authtest.py
@@ -6,14 +6,25 @@ class AuthtestData:
         pass
 
     def __activate__(self, context):
+        request = context["request"]
         response = context["response"]
-        writer = response.getPrintWriter("application/json; charset=UTF-8")
+        writer = response.getPrintWriter("text/javascript; charset=UTF-8")
         result = JsonConfigHelper()
+
+        ## Look for the JSONP callback to use
+        jsonpCallback = request.getParameter("callback")
+        if jsonpCallback is None:
+            jsonpCallback = request.getParameter("jsonp_callback")
+            if jsonpCallback is None:
+                response.setStatus(403)
+                writer.println("Error: This interface only responds to JSONP")
+                writer.close()
+                return
 
         if context["page"].authentication.is_logged_in():
             result.set("isAuthenticated", "true")
         else:
             result.set("isAuthenticated", "false")
 
-        writer.println(result.toString())
+        writer.println(jsonpCallback + "(" + result.toString() + ")")
         writer.close()
