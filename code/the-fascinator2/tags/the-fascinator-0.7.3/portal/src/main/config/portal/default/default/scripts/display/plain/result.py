a044f9c86ea78a4f23dfeb03b8c5c093ac9cc7ba
hudson <hudson@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2011-01-20 23:29:21 +0000

    [maven-release-plugin]  copy for tag the-fascinator-0.7.3

---
+++ code/the-fascinator2/tags/the-fascinator-0.7.3/portal/src/main/config/portal/default/default/scripts/display/plain/result.py
@@ -0,0 +1,30 @@
+from display.default.result import ResultData as DefaultResultData
+
+from java.io import ByteArrayOutputStream
+from org.apache.commons.io import IOUtils
+
+class ResultData(DefaultResultData):
+    def __activate__(self, context):
+        DefaultResultData.__activate__(self, context)
+
+    def getSourceSample(self, id, limit):
+        # Get source payload
+        object = self.services.getStorage().getObject(id)
+        if object is not None:
+            payload = object.getPayload(object.getSourceId())
+
+        # Read to a string
+        if payload is not None:
+            out = ByteArrayOutputStream()
+            IOUtils.copy(payload.open(), out)
+            payload.close()
+            string = out.toString("UTF-8")
+
+        # Return response
+        if string is not None:
+            if (len(string)) > limit:
+                return  string[0:limit] + "..."
+            else:
+                return  string
+        else:
+            return ""
