2cae3497b6633626ca24297c31d84070a1095cf4
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2011-01-27 01:49:09 +0000

    refs #1386 updated API version to 0.7.3 and added more javadoc

--- code/the-fascinator2/contrib/fascinator/plugins/storage/fedora3/trunk/src/main/java/au/edu/usq/fascinator/storage/fedora/Fedora3DigitalObject.java
+++ code/the-fascinator2/contrib/fascinator/plugins/storage/fedora3/trunk/src/main/java/au/edu/usq/fascinator/storage/fedora/Fedora3DigitalObject.java
@@ -1,3 +1,21 @@
+/*
+ * The Fascinator - Fedora Commons 3.x storage plugin
+ * Copyright (C) 2009-2011 University of Southern Queensland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package au.edu.usq.fascinator.storage.fedora;
 
 import java.io.File;
@@ -26,6 +44,11 @@ import au.edu.usq.fedora.types.DatastreamProfile;
 import au.edu.usq.fedora.types.DatastreamType;
 import au.edu.usq.fedora.types.ObjectDatastreamsType;
 
+/**
+ * Maps a Fedora object to a Fascinator digital object.
+ *
+ * @author Oliver Lucido
+ */
 public class Fedora3DigitalObject extends GenericDigitalObject {
 
     /** Logging */
