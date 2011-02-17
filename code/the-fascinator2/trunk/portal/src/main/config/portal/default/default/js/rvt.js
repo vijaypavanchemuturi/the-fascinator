d50c76e472f362f406ff66af760b2630d34df913
pendlebu <pendlebu@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2011-02-17 05:52:47 +0000

    Fixes #1483, fixes #1484. Fixed img rendering issues inside documents inside packages (including the organiser). Reduced the amount of 'redirect spam' coming from jQuery in RVT. Altered RVT to use page DOM rather then its own stack space when parsing to avoid memory errors for javascript engine.

--- code/the-fascinator2/trunk/portal/src/main/config/portal/default/default/js/rvt.js
+++ code/the-fascinator2/trunk/portal/src/main/config/portal/default/default/js/rvt.js
@@ -11,7 +11,6 @@ var rvtFactory = function(jQ) {
     var nodes;
 
     // Default configuration
-    rvt.fixRelativeLinks = true;
     rvt.contentBaseUrl = "";
     rvt.serverRequestTemplate = null;
     rvt.contentSelector = "#content";
@@ -31,11 +30,100 @@ var rvtFactory = function(jQ) {
     }
 
     // ====================================
+    // This method can be overridden. It will parse and process content
+    // just before it is added to the page. Intended for link fixing and similar
+    // ====================================
+    rvt.contentParser = function(content) {
+        var href, anchors = {};
+
+        // Get the second hash, the first is for the package
+        var a = window.location.hash.split("#",2).slice(1)[0];
+        // Make sure the base URI ends with a '/'
+        baseUri = rvt.contentBaseUrl;
+        if (baseUri[baseUri.length-1] !== "/") baseUri += "/";
+        // If there are paths in the hash, add them to the URI
+        baseUri += a.split("/").slice(0,-1).join("/");
+        // Again, make sure the base URI ends with a '/'
+        if (baseUri[baseUri.length-1] !== "/") baseUri += "/";
+
+        // ====================================
+        // Update paths found in the provided data with the baseUri.
+        function updateUri(node, attrName) {
+            var attribute = node.attr(attrName);
+            // Make sure we ignore absolute paths
+            function testIfLocalUri(uri) {
+                if (!uri) {return false;}
+                // If it starts with '/' or contains '://'
+                return uri.search(/^\/|\:\/\//g) === -1;
+            }
+            // If relative, add baseUri in front of the attribute
+            if (testIfLocalUri(a)){
+                node.attr(attrName, baseUri + attribute);
+            }
+        }
+
+        // Find every anchor element (we want the links)
+        content.find("a").each(function(c, a) {
+            // Get href attribute
+            a = jQ(a); href = a.attr("href");
+            if (href) {
+                // Does it have a hash link already?
+                if (href.substring(0,1) === "#") {
+                    // Push our hash in front
+                    a.attr("href", "#" + hash + href);
+                    // And record the anchor it targeted
+                    anchors[href.substring(1)] = hash + href;
+                } else {
+                    // Resolve this to work inside the package
+                    if (href = getHPath(href)) {
+                        a.attr("href", "#" + href);
+                    }
+                }
+            }
+        });
+
+        // Now for each anchor element (we want the targets this time)
+        content.find("a").each(function(c, a) {
+            var id, name;
+            a = jQ(a);
+            id = a.attr("id"); name = a.attr("name");
+            // Did we see it during the 'fix' above?
+            if (anchors[id]) {
+                // Modify the anchor name to match
+                a.attr("name", anchors[id]);
+            // Check if it has a name match too
+            } else if (anchors[name]) {
+                // Also update
+                a.attr("name", anchors[name]);
+            }
+        });
+
+        // Update anything with a 'src' attribute
+        content.find("*[src]").each(function(c, node){
+            node = jQ(node);
+            updateUri(node, "src");
+        });
+        // Or 'object's have a more complicated set of path possibilities
+        content.find("object").each(function(c, node){
+            node = jQ(node);
+            updateUri(node, "data");
+            updateUri(node, "codebase");
+            node.find("param[name='movie'],param[name='url'],param[name='media']").each(
+                function(c, node){
+                    node = jQ(node);
+                    updateUri(node, "value");
+                }
+            );
+        });
+    };
+
+    // ====================================
     // Update the on screen content in the package viewer.
     // ====================================
     rvt.updateContent = function (content) {
         var contentDiv = jQ(rvt.contentSelector);
         // Replace all old content
+        rvt.contentParser(content);
         contentDiv.html(content);
         // Remove the loading message if we loaded visible content
         if (content.is(":visible")) {
@@ -298,14 +386,20 @@ var rvtFactory = function(jQ) {
         // Some logic here is specifically looking for ICE
         // document structures to re-arrange.
         function callback(data) {
-            var href, anchors = {};
-            var pageToc = jQ(data).find("div.page-toc");
-            var body = jQ(data).find("div.body");
+            // Large documents will crash jQuery is parsed directly
+            //  so we farm them out to the browser by putting them in
+            //  the page under a hidden div
+            var jqData = $("<div/>");
+            jqData.hide();
+            jqData.html(data);
+
+            var pageToc = jqData.find("div.page-toc");
+            var body = jqData.find("div.body");
 
             // If ICE document structure not found, drop stright to screen
             if (body.size() == 0) {
                 pages[hash] = data;
-                rvt.updateContent(data);
+                rvt.updateContent(jQ(data));
                 rvt.contentScrollToTop();
                 return;
             }
@@ -313,102 +407,27 @@ var rvtFactory = function(jQ) {
             // Move ICE title in the document
             body.find("div.title").show().after(pageToc);
 
-            // If we're configured to fix links
-            if (rvt.fixRelativeLinks) {
-                // Find every anchor element (we want the links)
-                body.find("a").each(function(c, a) {
-                    // Get href attribute
-                    a = jQ(a); href = a.attr("href");
-                    if (href) {
-                        // Does it have a hash link already?
-                        if (href.substring(0,1) === "#") {
-                            // Push our hash in front
-                            a.attr("href", "#" + hash + href);
-                            // And record the anchor it targeted
-                            anchors[href.substring(1)] = hash + href;
-                        } else {
-                            // Resolve this to work inside the package
-                            if (href = getHPath(href)) {
-                                a.attr("href", "#" + href);
-                            }
-                        }
-                    }
-                });
-            }
-
-            // Now for each anchor element (we want the targets this time)
-            body.find("a").each(function(c, a) {
-                var id, name;
-                a = jQ(a);
-                id = a.attr("id"); name = a.attr("name");
-                // Did we see it during the 'fix' above?
-                if (anchors[id]) {
-                    // Modify the anchor name to match
-                    a.attr("name", anchors[id]);
-                // Check if it has a name match too
-                } else if (anchors[name]) {
-                    // Also update
-                    a.attr("name", anchors[name]);
-                }
-            });
-
-            // More fixing, this time for document hashes from the URL
-            var baseUri = "";
-            if (rvt.fixRelativeLinks) {
-                // Get the second hash, the first is for the package
-                var a = window.location.hash.split("#",2).slice(1)[0];
-                // Make sure the base URI ends with a '/'
-                baseUri = rvt.contentBaseUrl;
-                if (baseUri[baseUri.length-1] !== "/") baseUri += "/";
-                // If there are paths in the hash, add them to the URI
-                baseUri += a.split("/").slice(0,-1).join("/");
-                // Again, make sure the base URI ends with a '/'
-                if (baseUri[baseUri.length-1] !== "/") baseUri += "/";
-            }
-
-            // ====================================
-            // Update paths found in the provided data with the baseUri.
-            function updateUri(node, attrName) {
-                var attribute = node.attr(attrName);
-                // Make sure we ignore absolute paths
-                function testIfLocalUri(uri) {
-                    if (!uri) {return false;}
-                    // If it starts with '/' or contains '://'
-                    return uri.search(/^\/|\:\/\//g) === -1;
-                }
-                // If relative, add baseUri in front of the attribute
-                if (testIfLocalUri(a)){
-                    node.attr(attrName, baseUri + attribute);
-                }
-            }
-
-            // Update anything with a 'src' attribute
-            body.find("*[src]").each(function(c, node){
-                node = jQ(node);
-                updateUri(node, "src");
-            });
-            // Or 'object's have a more complicated set of path possibilities
-            body.find("object").each(function(c, node){
-                node = jQ(node);
-                updateUri(node, "data");
-                updateUri(node, "codebase");
-                node.find("param[name='movie'],param[name='url'],param[name='media']").each(
-                    function(c, node){
-                        node = jQ(node);
-                        updateUri(node, "value");
-                    }
-                );
-            });
-
             // For content, we just want div children of the body
             var html = body.find(">div");
+
+            // Oops, this should rarely happen, it means the ICE document
+            //  has an unexpected page strucutre at the top level.
+            if (html.size() == 0) {
+                // Just push the html to screen
+                pages[hash] = data;
+                rvt.updateContent(body);
+                rvt.contentScrollToTop();
+                return;
+            }
+
             // Move any rendition links down into the body of the
             // document so they aren't lost.
-            jQ(data).find("div.rendition-links").each(
+            jqData.find("div.rendition-links").each(
                 function(c, node){
                     html.prepend("<div class='rendition-links'><span class='heading'>Renditions</span>" + unescape(jQ(node).html()) + "</div>");
                 }
             );
+
             // Cache the content after all that parsing
             pages[hash] = html;
             // And update the page
