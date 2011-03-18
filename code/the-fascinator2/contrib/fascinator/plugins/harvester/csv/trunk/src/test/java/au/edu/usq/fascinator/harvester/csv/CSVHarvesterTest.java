b1b55965dd1caa0f530dccaca442f48050263031
lucido <lucido@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2011-03-18 06:06:55 +0000

    refs #1545 updated to use the new JSON libraries and supports incremental processing with configurable batch size (instead of single request processing all rows)

--- code/the-fascinator2/contrib/fascinator/plugins/harvester/csv/trunk/src/test/java/au/edu/usq/fascinator/harvester/csv/CSVHarvesterTest.java
+++ code/the-fascinator2/contrib/fascinator/plugins/harvester/csv/trunk/src/test/java/au/edu/usq/fascinator/harvester/csv/CSVHarvesterTest.java
@@ -1,5 +1,5 @@
 /* 
- * The Fascinator - Plugin - Harvester - File System
+ * The Fascinator - Plugin - Harvester - CSV
  * Copyright (C) 2009 University of Southern Queensland
  * 
  * This program is free software: you can redistribute it and/or modify
@@ -19,13 +19,11 @@
 package au.edu.usq.fascinator.harvester.csv;
 
 import java.io.File;
-import java.util.Map;
 import java.util.Set;
 
 import junit.framework.Assert;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,62 +33,99 @@ import au.edu.usq.fascinator.api.harvester.Harvester;
 import au.edu.usq.fascinator.api.storage.Storage;
 
 /**
- * Unit tests for the CSV harvester plugin
+ * Unit tests for the CSV harvester plugin.
  * 
  * @author Linda Octalina
  */
 public class CSVHarvesterTest {
 
-    /** logging */
+    /** Logging */
     private Logger log = LoggerFactory.getLogger(CSVHarvesterTest.class);
 
+    /** In memory storage */
     private Storage ram;
 
     /**
      * Sets the "test.dir" and "test.cache.dir" system property for use in the
-     * JSON configuration
+     * JSON configuration.
      * 
-     * @throws Exception if any error occurs
+     * @throws Exception if any error occurred
      */
     @Before
     public void setup() throws Exception {
-        File baseDir = new File(CSVHarvesterTest.class.getResource("/")
-                .toURI());
+        File baseDir = new File(CSVHarvesterTest.class.getResource("/").toURI());
         System.setProperty("test.dir", baseDir.getAbsolutePath());
-
         ram = PluginManager.getStorage("ram");
         ram.init("{}");
     }
 
     /**
-     * Test on getting object list
+     * Test getting a list of object identifiers.
      * 
-     * @throws Exception if any error occurs
+     * @throws Exception if any error occurred
      */
     @Test
-    public void getObjectIdList() throws Exception {
-        CSVHarvester csvHarvester = getHarvester("/csv-config.json");
-        Set<String> geonames = csvHarvester.getObjectIdList();
-        Assert.assertEquals(4, geonames.size());
-    }
-    
-    /**
-     * Test on getting object list
-     * 
-     * @throws Exception if any error occurs
-     */
-    @Test
-    public void getObjectIdListNoHeader() throws Exception {
-        CSVHarvester csvHarvester = getHarvester("/csv-no-header-config.json");
-        Set<String> geonames = csvHarvester.getObjectIdList();
-        Assert.assertEquals(5, geonames.size());
+    public void simple() throws Exception {
+        CSVHarvester csvHarvester = getHarvester("/simple.json");
+        Set<String> idList = csvHarvester.getObjectIdList();
+        Assert.assertEquals(4, idList.size());
     }
 
-    private CSVHarvester getHarvester(String filename) throws Exception {
-        Harvester csvHarvester = PluginManager.getHarvester("csv",
-                ram);
-        File f = new File(getClass().getResource(filename).toURI());
-        csvHarvester.init(f);
+    /**
+     * Test getting a list of object identifiers.
+     *
+     * @throws Exception if any error occurred
+     */
+    @Test
+    public void simpleBatch() throws Exception {
+        CSVHarvester csvHarvester = getHarvester("/simple-batch.json");
+        int count = 0;
+        while (csvHarvester.hasMoreObjects()) {
+            Set<String> idList = csvHarvester.getObjectIdList();
+            count += idList.size();
+        }
+        Assert.assertEquals(4, count);
+    }
+
+    /**
+     * Test getting a list of object identifiers with no header row.
+     * 
+     * @throws Exception if any error occurred
+     */
+    @Test
+    public void simpleNoHeader() throws Exception {
+        CSVHarvester csvHarvester = getHarvester("/geonames.json");
+        Set<String> idList = csvHarvester.getObjectIdList();
+        Assert.assertEquals(5, idList.size());
+    }
+
+    /**
+     * Test getting a list of object identifiers with a slightly more complex
+     * csv file.
+     *
+     * @throws Exception if any error occurred
+     */
+    @Test
+    public void complex() throws Exception {
+        CSVHarvester csvHarvester = getHarvester("/complex.json");
+        Set<String> idList = csvHarvester.getObjectIdList();
+        for (String oid : idList) {
+            log.debug("{}", oid);
+        }
+        Assert.assertEquals(4, idList.size());
+    }
+
+    /**
+     * Gets a CSV harvester instance and initialises it with the specified
+     * configuration file.
+     *
+     * @param configFile JSON configuration file
+     * @return CSV harvester
+     * @throws Exception if any error occurred
+     */
+    private CSVHarvester getHarvester(String configFile) throws Exception {
+        Harvester csvHarvester = PluginManager.getHarvester("csv", ram);
+        csvHarvester.init(new File(getClass().getResource(configFile).toURI()));
         return (CSVHarvester) csvHarvester;
     }
 }
