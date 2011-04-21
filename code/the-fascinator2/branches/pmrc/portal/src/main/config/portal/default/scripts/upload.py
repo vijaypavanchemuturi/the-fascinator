ee74f8f3f18ff3e8e337a34765f7168ff04e2e2d
dickinso <dickinso@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2011-04-21 05:07:19 +0000

    Not needed

--- code/the-fascinator2/branches/pmrc/portal/src/main/config/portal/default/scripts/upload.py
+++
@@ -1,9 +0,0 @@
-class UploadData:
-
-    def __init__(self):
-        self.uploader = toolkit.getFileUploader()
-
-    def render_upload_form(self):
-        return self.uploader.renderForm()
-
-scriptObject = UploadData()
