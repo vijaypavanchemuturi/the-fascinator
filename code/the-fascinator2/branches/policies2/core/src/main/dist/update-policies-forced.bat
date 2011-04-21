22a039f43a216b4427014d03fb7c14e7eaeeecd4
dickinso <dickinso@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2011-04-21 05:07:55 +0000

    Not needed

--- code/the-fascinator2/branches/policies2/core/src/main/dist/update-policies-forced.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-java -Dhttp.proxyHost=proxy.usq.edu.au -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=localhost -cp "C:\Program Files\The Fascinator\lib\*" au.edu.usq.fascinator.HarvestClient "C:\Program Files\The Fascinator\rules\usq-policies-forced-windows.json" > "C:\Program Files\The Fascinator\update-forced.log"
-echo The process has been logged to C:\Program Files\The Fascinator\update-forced.log
-pause
