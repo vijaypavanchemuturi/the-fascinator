7be79f1f796d451da0821c7a705a96a34fab594b
greg.pendlebury@gmail.com <greg.pendlebury@gmail.com@f51757dd-c7a7-c463-22b4-a0e335d354ad>
2011-07-07 02:29:48 +0000

    Preparing for v1.0.3 release into Sonatype

--- the-fascinator/contrib/fascinator/plugins/transformer/jsonVelocity/trunk/src/main/java/com/googlecode/fascinator/transformer/jsonVelocity/LoggingWrapper.java
+++ the-fascinator/contrib/fascinator/plugins/transformer/jsonVelocity/trunk/src/main/java/com/googlecode/fascinator/transformer/jsonVelocity/LoggingWrapper.java
@@ -16,7 +16,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package au.edu.usq.fascinator.transformer.jsonVelocity;
+package com.googlecode.fascinator.transformer.jsonVelocity;
 
 import org.apache.velocity.app.Velocity;
 import org.apache.velocity.runtime.RuntimeServices;
