b5cb47a77b4d5719edc890555d4f4f9b3f45b6e0
duncan.qcif <duncan.qcif@gmail.com>
2012-09-12 03:51:55 +0000

    Version as at time of migration to GitHub

---
+++ watcher/tags/git-migration/app/controller.py
@@ -0,0 +1,261 @@
+#
+#    Copyright (C) 2009  ADFI,
+#    University of Southern Queensland
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+import time
+
+
+class Controller(object):
+    # Constructor:
+    #   Controller(db, fileSystem, config, FileWatcher, WatchDirectory, 
+    #                                   update=True)
+    # Properties:
+    #   watchDirectories
+    # Methods:
+    #   configChanged(config)
+    #   startWatching(watchDirectory)
+    #   stopWatching(watchDirectory)
+    #   getRecordsFromDate(fromDate=0, toDate=None)
+    #   addListener(listener)
+    #   removeListener(listener)
+    #   close()
+    #   _getRecordsCount(startingWithPath="")
+    #   _updateWalk(watchDirectory)
+    #   _updateHandler(file, eventTime, eventName, isDir=False)
+    def __init__(self, db, fileSystem, config, FileWatcher, WatchDirectory, \
+                        update=True):
+        self.__db = db
+        self.__fs = fileSystem          # .getModifiedTime(path), .walker(path, callback)
+        self.__FileWatcher = FileWatcher        # is a FileSystemWatcher
+        self.__WatchDirectory = WatchDirectory
+        self.__watchDirectories = self.__getWatchDirectoriesFromConfig(config)
+        self.__listeners = []
+        #config.addConfigChangeWatcher(self.__)
+        for watchDirectory in self.__watchDirectories.values():
+            # check to see if we are conintue watching or are starting watching
+            if not watchDirectory.stop:
+                if self.__isStartWatch(watchDirectory):
+                    self.startWatching(watchDirectory)
+                else:
+                    self.__watch(watchDirectory)
+                    if update:
+                        self._updateWalk(watchDirectory)
+
+
+    @property
+    def watchDirectories(self):
+        return self.__watchDirectories
+
+
+    def configChanged(self, config):
+        # look for watchDirectories to Start and Stop watching
+        watchDirectories = self.__getWatchDirectoriesFromConfig(config)
+        currentWatchedPaths = self.__watchDirectories.keys()
+        for wd in watchDirectories.itervalues():
+            cwd = self.__watchDirectories.get(wd.path)
+            if cwd is None:
+                self.startWatching(wd)
+            else:
+                currentWatchedPaths.remove(wd.path)
+                if wd==cwd:
+                    # no change
+                    pass
+                else:
+                    # OK just update the filters
+                    cwd.ignoreFileFilter = wd.ignoreFileFilter
+                    cwd.ignoreDirectories = wd.ignoreDirectories
+        for path in currentWatchedPaths:
+            cwd = self.__watchDirectories.get(path)
+            self.stopWatching(cwd)
+
+
+    def startWatching(self, watchDirectory):
+        ## Remove all paths that are watched by any other watchDirectories
+        self.__watchDirectories[watchDirectory.path] = watchDirectory
+        self.__watch(watchDirectory)
+        self._updateWalk(watchDirectory, startEvent=True)
+
+    
+    def stopWatching(self, watchDirectory):
+        print "stopWatching '%s'" % watchDirectory.path
+        if self.__watchDirectories.has_key(watchDirectory.path):
+            self.__watchDirectories.pop(watchDirectory.path)
+        watchDirectory.watcher = None       # will also stop watching and close
+        #
+        rows = self.__db.getRecordsStartingWithPath(watchDirectory.path[:-1])
+        timeNow = self.__timeNow()
+        ## Remove all paths that are watched by any other watchDirectories
+        #for opath in self.__watchDirectories.keys():
+        #    if watchDirectory.path.startswith(opath):
+        #        rows = [row for row in rows if row[0].startswith(opath)]
+        for row in rows:
+            file, eventTime, eventName, isDir = row
+            if eventName=="mod":
+                eventName = "stopmod"
+            elif eventName=="del":
+                eventName = "stopdel"
+            else:
+                eventName = "stop"
+            self._updateHandler(file, timeNow, eventName, isDir)
+    
+    
+    def getRecordsFromDate(self, fromDate=0, toDate=None):
+        rows = self.__db.getRecordsFromDate(fromDate, toDate)
+        return rows
+    
+    
+    def addListener(self, listener):
+        self.__listeners.append(listener)
+    
+    def removeListener(self, listener):
+        if self.__listeners.count(listener)>0:
+            self.__listeners.remove(listener)
+            return True
+        return False
+    
+    
+    def close(self):
+        for watchDirectory in self.__watchDirectories.itervalues():
+            watcher = watchDirectory.watcher
+            if watcher is not None:
+                watcher.close()
+        self.__db.close()
+
+
+    def _getRecordsCount(self, startingWithPath=""):
+        rows = self.__db.getRecordsStartingWithPath(startingWithPath)
+        return len(rows)
+
+
+    def _updateWalk(self, watchDirectory, startEvent=False):
+        # run this in its own thread
+        rows = self.__db.getRecordsStartingWithPath(watchDirectory.path[:-1])
+        dfiles = dict([(row[0], row) for row in rows])
+        timeNow = self.__timeNow()
+
+        def callback(path, dirs, files):
+            path = path.replace("\\", "/")
+            if watchDirectory.filter(path)==False:
+                while len(dirs)>0:
+                    dirs.pop()
+                return
+            if startEvent:
+                ## remove any other watchDirectories - NO - is still a start watch for this watched path
+                etime = timeNow
+                eventName = "start"
+            else:
+                etime = self.__fs.getModifiedTime(path)
+                eventName = "mod"
+            self._updateHandler(path[:-1], etime, eventName, True)
+            if dfiles.has_key(path[:-1]):
+                dfiles.pop(path[:-1])
+            for file in files:
+                fullFile = path + file
+                if watchDirectory.filter(fullFile)==False:
+                    continue
+                if startEvent:
+                    etime = timeNow
+                    eventName = "start"
+                else:
+                    etime = self.__fs.getModifiedTime(fullFile)
+                    eventName = "mod"
+                self._updateHandler(fullFile, etime, eventName, False)
+                if dfiles.has_key(fullFile):
+                    dfiles.pop(fullFile)
+        self.__fs.walker(watchDirectory.path, callback)
+        # what is left must have been deleted
+        for file, _, _, isDir in dfiles.itervalues():
+            print "deleting '%s'" % file
+            self._updateHandler(file, timeNow, "del", isDir)
+
+
+    def _updateHandler(self, file, eventTime, eventName, isDir=False):
+        self.__updatedb([(file, eventTime, eventName, isDir)])
+        for listener in self.__listeners:
+            if callable(listener):
+                try:
+                    listener(file, eventTime, eventName, isDir)
+                except Exception, e:
+                    pass
+
+
+    def __updatedb(self, uList):
+        try:
+            self.__db.updateList(uList)
+        except Exception, e:
+            print "ERROR in db.updateList() - '%s'" % str(e)
+    
+    
+    def __watch(self, watchDirectory):
+        #print "__watch '%s'" % watchDirectory
+        try:
+            watcher = self.__FileWatcher(watchDirectory.path, self.__fs, \
+                    self.__getChildrenOf, self.__db.getRecordWithPath)
+            watchDirectory.watcher = watcher
+            watcher.addListener(watchDirectory.updateHandler)
+            watchDirectory.addListener(self._updateHandler)
+            watcher.startWatching()
+        except Exception, e:
+            msg = str(e)
+            if msg.find("Directory does not exists")!=-1:
+                msg = "Directory does not exists!"
+            print "Failed to start watching '%s' - %s" % (watchDirectory.path, msg)
+    
+            
+    def __getChildrenOf(self, path):
+        rows = self.__db.getAllActiveChildrenOfPath(path)
+        return [(file, isDir) for file, et, en, isDir in rows]
+    
+    
+    def __isStartWatch(self, watchDirectory):
+        # is a 'start watching' if this path does not exist in the database
+        #       or it's eventName starts with 'stop'
+        row = self.__db.getRecordWithPath(watchDirectory.path[:-1])
+        return row is None or row[2].startswith("stop")
+
+
+    def __getWatchDirectoriesFromConfig(self, config):
+        watchDirectories = {}
+        try:
+            for path, d in config.watcher.watchDirs.iteritems():
+                path = self.__fs.absPath(path)
+                #print "path='%s', d='%s'" % (path, d)
+                ignoreFileFilter = d.get("ignoreFileFilter", "")
+                ignoreDirectories = d.get("ignoreDirectories", "")
+                stop = d.get("stop", 0)
+                cxtTags = d.get("cxtTags", [])
+                watchDir =  self.__WatchDirectory(path)
+                watchDir.ignoreFileFilter = ignoreFileFilter
+                watchDir.ignoreDirectories = ignoreDirectories
+                watchDir.stop = stop
+                watchDir.cxtTags = cxtTags
+                watchDirectories[watchDir.path] = watchDir
+        except Exception, e:
+            print str(e)
+        return watchDirectories
+
+
+    def __timeNow(self):
+        return int(time.time())
+
+
+        
+        
+        
+        
+        
---
+++ watcher/tags/git-migration/app/webServer.py
@@ -0,0 +1,221 @@
+#
+#    Copyright (C) 2009  ADFI,
+#    University of Southern Queensland
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+import BaseHTTPServer
+import os
+import sys
+import threading
+import time
+from urllib import unquote
+
+
+
+class ServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    feeder = None
+    httpd = None
+    def do_GET(self):
+        fromDate = self.headers.getheader("Last-Modified")
+        toDate = None
+        path = unquote(self.path).replace("+", " ")
+        fromDate, toDate, cmd = self.__getFromPath(path, fromDate)
+
+        if cmd=="test":
+            self.__test(path, fromDate, toDate)
+        elif cmd=="count":
+            self.__getCount()
+        elif cmd=="html":
+            self.__getHtmlFeed(fromDate, toDate)
+        elif cmd=="text":
+            self.__getTextFeed(fromDate, toDate)
+        elif cmd=="name":
+            self.__name()
+        elif cmd=="time":
+            self.__time()
+        elif cmd=="shutdown":
+            self.shutdown()
+        else:
+            self.__getJsonFeed(fromDate, toDate)
+
+    def __getFromPath(self, path, fromDate):
+        toDate = None
+        cmd = None
+        parts = path.split("/")
+        for part in parts:
+            if part=="":
+                continue
+            if part[0].isdigit():
+                if fromDate is None:
+                    fromDate = part
+                elif toDate is None:
+                    toDate = part
+            elif part.isalpha():
+                if cmd is None:
+                    cmd = part
+        if fromDate is None:
+            fromDate = "1970-01-01"
+        return fromDate, toDate, cmd
+
+    def __test(self, path, fromDate, toDate):
+        self.send_response(200, "OK")
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        s = "TEST path='%s', fromDate='%s', toDate='%s'" % (path, fromDate, toDate)
+        s += "\n"
+        if fromDate is not None:
+            try:
+                secs = self.feeder.convertGMTToInteger(fromDate)
+            except:
+                secs = "?"
+            s += ("fromDate='%s'  Secs=%s\n" % (fromDate, secs))
+        if toDate is not None:
+            try:
+                secs = self.feeder.convertGMTToInteger(toDate)
+            except:
+                secs = "?"
+            s += ("toDate='%s'  Secs=%s\n" % (toDate, secs))
+        print s
+        s = s.replace("\n", "<br/>")
+        self.wfile.write(s)
+
+    def __getJsonFeed(self, fromDate, toDate):
+        #lastModifiedFile = self.feeder.lastModifiedTimeStamp()
+        def compare(a, b):
+            return cmp(a[1], b[1])
+        try:
+            lastModified = self.feeder.formatDateTime(time.time(), utc=True)
+            rows = self.feeder.getFeed(fromDate, toDate)
+            rows.sort(compare)
+            if rows==[]:
+                t = self.feeder.convertGMTToInteger(fromDate)
+            else:
+                t = rows[-1][1]
+            try:
+                lastModified = self.feeder.formatDateTime(t, utc=True)
+            except:
+                i = self.feeder.convertGMTToInteger(t)
+                t = self.feeder.formatDateTime(i, utc=True)
+                lastModified = t
+            data = rows
+            # change to a dictionary of dictionaries  e.g. {file:{time:.., state:...}, ...}
+            data = dict([(f, {"time":t, "state":s}) for f, t, s in rows])
+            try:
+                data =self.feeder.convertToJson(data)
+            except Exception, e:
+                print "__getJsonFeed   %s" % str(e)
+                data = str(data)
+        except Exception, e:
+            print str(e)
+            data = str({"Error": str(e)})
+        self.send_response(200, "OK")
+        self.send_header("Content-type", "application/json")
+        #self.send_header("Content-type", "text/html")
+        self.send_header("Last-Modified", lastModified)
+        self.end_headers()
+        self.wfile.write(data)
+
+
+    def __getHtmlFeed(self, fromDate, toDate):
+        data = self.__getFeedText(fromDate, toDate)
+        self.__write(data, contentType="text/html")
+
+    def __getTextFeed(self, fromDate, toDate):
+        data = self.__getFeedText(fromDate, toDate)
+        self.__write(data, contentType="text/plain")
+
+    def __getFeedText(self, fromDate, toDate):
+        def compare(a, b):
+            return cmp(a[1], b[1])
+        try:
+            rows = self.feeder.getFeed(fromDate, toDate, True)
+            rows.sort(compare)
+            data = ""
+            for file, eventTime, eventName, isDir in rows:
+                data += "%s, %s, %s, %s\n" % (eventTime, eventName, file, isDir)
+            data = BaseHTTPServer._quote_html(data)
+            #if True:
+            try:
+                if rows==[]:
+                    lastModified = fromDate
+                    #lastModified = self.feeder.formatDateTime(fromDate, utc=True)
+                else:
+                    lastModified = rows[-1][1]
+                data += "\nlastModified = %s" % lastModified
+            except Exception, e:
+                print str(e)
+                print "t='%s'" % t
+        except Exception, e:
+            data = str({"Error": str(e)})
+        return data
+
+    def __getCount(self, path="/"):
+        try:
+            print "getCount path='%s'" % path
+            data = "FileCount=%s" % self.feeder.getFileCount(path)
+        except Exception, e:
+            data = str({"Error": str(e)})
+        self.__write(data)
+
+    def __name(self):
+        name = os.environ.get("COMPUTERNAME", "?")
+        self.__write("ComputerName='%s', platform='%s'" % (name, sys.platform))
+
+    def __time(self):
+        data = "CurrentTime=%s" % self.feeder.formatDateTime()
+        data += ", GMT = %s" % self.feeder.formatDateTime(utc=True)
+        self.__write(data)
+
+    def __write(self, data, contentType="text/html"):
+        self.send_response(200, "OK")
+        self.send_header("Content-type", contentType)
+        self.end_headers()
+        self.wfile.write(data)
+
+    def shutdown(self):
+            print "shutdown requested"
+            self.__write("Shutdown")
+            def shutdown():
+                try:
+                    self.httpd.shutdown()
+                except:
+                    pass
+            threading.Timer(0.2, shutdown).start()
+
+
+
+def webServe(host, port, feeder):
+    ServerHandler.feeder = feeder       ####################
+    httpd = BaseHTTPServer.HTTPServer( (host, port), ServerHandler)
+    ServerHandler.httpd = httpd
+    running = [False]
+    def shutdown():
+        if running[0]:
+            try:
+                httpd.shutdown()
+            except Exception, e:
+                print str(e)
+                print " try pressing Ctrl+C a couple of times to shutdown"
+    
+    def serve():
+        running[0] = True
+        httpd.serve_forever()
+        running[0] = False
+    threading.Thread(target=serve, name="webServer").start()
+    return shutdown
+
+    
